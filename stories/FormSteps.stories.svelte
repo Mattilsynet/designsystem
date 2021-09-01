09b518661b979530dc3e8c94aa6e48a6f4a30d9c
Tom Arild Jakobsen <tajakobsen@gmail.com>
2021-09-01 22:03:59 +0200

    Add form progress step component
    Relates to: MT-148

---
+++ stories/FormSteps.stories.svelte
@@ -0,0 +1,37 @@
+<script lang='ts'>
+  import {Meta, Story} from '@storybook/addon-svelte-csf';
+
+  const steps = [
+    'Om bekymringen',
+    'Info om deg',
+    'Oppsummering',
+    'Bekreftelse'
+  ];
+</script>
+
+<Meta
+  title='Components/Steps'
+  args={{
+    completed: 2
+  }}
+  argTypes={{
+    completed: {control: 'number'},
+  }} />
+
+<Story name='Normal' let:completed>
+  <ol class='steps'>
+    {#each steps as step, index (step)}
+      <li class:steps__complete={ index < completed }>
+        {#if index < completed }
+          <span class='inclusively-hidden'>Ferdig: </span>
+        {/if}
+
+        {step}
+      </li>
+    {/each}
+  </ol>
+</Story>
+
+<style lang='scss' global>
+  @import "../src/scss/app";
+</style>
