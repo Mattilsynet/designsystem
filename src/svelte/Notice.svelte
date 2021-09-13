690853e049c2695e05cb81cb7ba7d43f251637da
Lisa <halvorsen.lisa@gmail.com>
2021-09-13 15:32:29 +0200

    Add notice component (#19)
    Relates to: MT-204

---
+++ src/svelte/Notice.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let infoType: 'important' | 'none';
+  export let iconText: string | undefined;
+  export let text: string;
+</script>
+
+<div class="notice">
+  {#if infoType !== 'none'}
+    <span class={`${infoType}`} />
+  {/if}
+  {#if iconText}
+    <span class="inclusively-hidden">{iconText}</span>
+  {/if}
+  <div class="text">
+    {@html text}
+  </div>
+</div>
---
+++ stories/components/Notice.stories.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import {Meta, Story} from '@storybook/addon-svelte-csf';
+  import Notice from '../../src/svelte/Notice.svelte';
+</script>
+
+<Meta
+  title="Components/Notice"
+  args={{
+    text: '<p>Haster det? Er det fare for at dyr dør eller blir påført store belastninger. Ring mattilsynet på tlf. <a href="tel:22400000">22 40 00 00</a> eller politiet <a href="tel:02800">02800</a></p>',
+    disabled: false,
+    disableJs: false,
+    disableCss: false
+  }}
+  argTypes={{
+    disabled: {control: 'boolean'},
+    disableJs: {control: 'boolean'},
+    disableCss: {control: 'boolean'}
+  }}
+/>
+
+<Story name="Normal" let:text let:disableCss>
+  <main class="container layout-flex-col">
+    <h1>Varseltekst</h1>
+    <section class="layout-flex-col">
+      <h2>Med varsel icon</h2>
+      <Notice infoType="important" {text} />
+    </section>
+    <section class="layout-flex-col">
+      <h2>Uten icon</h2>
+      <Notice infoType="none" {text} />
+    </section>
+  </main>
+</Story>
+
+<style lang="scss">
+  @import 'src/scss/app';
+  main {
+    --gap: var(--spacer-large);
+  }
+  section {
+    --gap: var(--spacer-small);
+  }
+</style>
