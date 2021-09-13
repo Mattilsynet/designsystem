c13bfdeb73709f115cf9099a834acbb1c97fc413
Lisa <halvorsen.lisa@gmail.com>
2021-09-13 13:01:41 +0200

    Add notice component
    Relates to MT-204

---
+++ src/svelte/Notice.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let infoType: 'important' | 'none';
+  export let iconText: string | undefined;
+  export let text: string;
+</script>
+
+<div class="notice">
+  <span class={`icon ${infoType}`} />
+  {#if iconText}
+    <span class="inclusively-hidden">{iconText}</span>
+  {/if}
+  <div class="text">
+    {@html text}
+  </div>
+</div>
