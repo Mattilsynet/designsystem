21d1c032b7cf484c662a773e85e40acfc74ad5a8
Lisa <halvorsen.lisa@gmail.com>
2021-09-14 11:14:46 +0200

    Update styling for notice
    - to provide some styling for content from xp html area
    - reface style to mixin
    
    Relates to MT-186

--- stories/components/Notice.stories.svelte
+++ stories/components/Notice.stories.svelte
@@ -6,7 +6,12 @@
 <Meta
   title="Components/Notice"
   args={{
-    text: '<p>Haster det? Er det fare for at dyr dør eller blir påført store belastninger. Ring mattilsynet på tlf. <a href="tel:22400000">22 40 00 00</a> eller politiet <a href="tel:02800">02800</a></p>',
+    text: `<p>Haster det? Er det fare for at dyr dør eller blir påført store belastninger. Ring mattilsynet på tlf. <a href="tel:22400000">22 40 00 00</a> eller politiet <a href="tel:02800">02800</a></p>`,
+    text2: `<p>Haster det? Er det fare for at dyr dør eller blir påført store belastninger</p> <ul>
+            <li>Hvor skjedde det?</li>
+            <li>Her er andre punkt i listen</li>
+          </ul>
+<p>Ekstra paragraph</p>`,
     disabled: false,
     disableJs: false,
     disableCss: false
@@ -18,12 +23,13 @@
   }}
 />
 
-<Story name="Normal" let:text let:disableCss>
+<Story name="Normal" let:text let:text2 let:disableCss>
   <main class="container layout-flex-col">
     <h1>Varseltekst</h1>
     <section class="layout-flex-col">
       <h2>Med varsel icon</h2>
       <Notice infoType="important" {text} />
+      <Notice infoType="important" text={text2} />
     </section>
     <section class="layout-flex-col">
       <h2>Uten icon</h2>
