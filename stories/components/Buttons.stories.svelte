c05ede34e3997f12242fc10b039ce24866332baa
Øyvind Dahl <oyvind.dahl@item.no>
2021-09-22 15:28:19 +0200

    Qa

--- stories/components/Buttons.stories.svelte
+++ stories/components/Buttons.stories.svelte
@@ -2,7 +2,7 @@
   import {Meta, Story} from '@storybook/addon-svelte-csf';
   import {action} from '@storybook/addon-actions';
   import FileUploadButton from '../../src/svelte/FileUploadButton.svelte';
-  import Button from '../../src/svelte/Button.svelte';
+  import Button from '../../src/svelte/ButtonSpinner.svelte';
 </script>
 
 <Meta
@@ -46,7 +46,19 @@
 </Story>
 
 <Story name="Loading" >
-  <Button progressIndicator={true}>Send in skjema</Button>
+  <h1>Loading</h1>
+  <p class="description">Hovedknapp</p>
+  <Button formInProgressAriaLabel='Sender inn skjema, venter på svar.' spinnerPlacement='start' color='primary'>
+    Spinner placement start
+  </Button><br/>
+  <Button formInProgressAriaLabel='Sender inn skjema, venter på svar.' spinnerPlacement='end' color='primary'>
+    Spinner placement end
+  </Button>
+
+  <p class="description">Sekundær</p>
+  <Button formInProgressAriaLabel='Sender inn skjema, venter på svar.' spinnerPlacement='end' color='secondary'>
+    Send in skjema
+  </Button>
 </Story>
 
 <style lang="scss" global>
