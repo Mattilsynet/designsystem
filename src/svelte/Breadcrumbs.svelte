abd99fca61e5c3574412c5ff0fff89172da2a7e9
Lisa <halvorsen.lisa@gmail.com>
2021-09-24 10:29:55 +0200

    Componentify Breadcrumbs for reuse
    Relates to MT-215

---
+++ src/svelte/Breadcrumbs.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  interface Breadcrumbs {
+    items: [{url: string; title: string}];
+  }
+
+  export let breadcrumbs: Breadcrumbs;
+  export let ariaLabel: string = 'breadcrumbs';
+</script>
+
+<nav class="breadcrumbs" aria-label={ariaLabel}>
+  <ol>
+    {#each breadcrumbs.items as item, index}
+      <li>
+        {#if index + 1 < breadcrumbs.items.length}
+          <a class="forward-arrow" href={item.url}>{item.title}</a>
+        {:else}
+          <a aria-current="page" href={item.url}>{item.title}</a>
+        {/if}
+      </li>
+    {/each}
+  </ol>
+</nav>
