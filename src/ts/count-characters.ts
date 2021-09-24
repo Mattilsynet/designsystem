4ab0a114b8c3d19c7ea9d71193e4b1a4928a36f1
Tom Arild Jakobsen <tajakobsen@gmail.com>
2021-09-24 15:24:16 +0200

    Add character counter for input and textarea
    Relates to: MT-327

---
+++ src/ts/count-characters.ts
@@ -0,0 +1,24 @@
+import {Action} from './types';
+
+export const countCharacters: Action<HTMLInputElement | HTMLTextAreaElement> = node => {
+  const counterEl = document.createElement('div');
+  counterEl.classList.add('self-end');
+
+  function updateCounter() {
+    counterEl.innerText = `${node.maxLength - node.value.length} tegn igjen`;
+  }
+
+  if (node.hasAttribute('maxlength')) {
+    updateCounter();
+    node.after(counterEl);
+    node.addEventListener('input', updateCounter);
+
+    return {
+      destroy() {
+        node.removeEventListener('input', updateCounter);
+      }
+    };
+  }
+
+  return;
+};
--- src/ts/types.d.ts
+++ src/ts/types.d.ts
@@ -2,3 +2,11 @@ export interface UIError {
   fieldName: string;
   message: string;
 }
+
+export type Action<Node extends HTMLElement> = (
+  node: Node,
+  parameters: any
+) => {
+  update?: (parameters: any) => void;
+  destroy?: () => void;
+} | void;
