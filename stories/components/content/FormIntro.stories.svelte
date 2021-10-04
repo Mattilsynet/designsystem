6be29d12ebac23f236e91502a47cdbf2eb308889
Lisa <halvorsen.lisa@gmail.com>
2021-10-04 13:03:01 +0200

    Add styling and story for form intro module (#41)
    Relates to MT-348

---
+++ stories/components/content/FormIntro.stories.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import {Meta, Story} from '@storybook/addon-svelte-csf';
+</script>
+
+<Meta
+  title="Components/Innhold/Skjemaforside (modul)"
+  args={{
+    title: 'Varsle om piggtrådgjerder',
+    intro: '',
+    text: `<p>Du bør varsle Mattilsynet hvis du oppdager</p>
+<ul>
+ <li>at noen setter opp nye piggtrådgjerder</li>
+ <li>eldre piggtrådgjerder som utgjør en risiko for at dyr blir skadet</li>
+ </ul>`,
+    formLinkUrl: 'http://',
+    formLinkText: 'Lenke til skjema',
+    disableCss: false
+  }}
+  argTypes={{
+    disabled: {control: 'boolean'},
+    disableCss: {control: 'boolean'}
+  }}
+/>
+
+<Story name="Normal" let:title let:intro let:text let:moreText let:formLinkUrl let:formLinkText>
+  <div class="container">
+    <article class="form-intro">
+      <h3>{title}</h3>
+
+      {#if intro}
+        <div class="intro">
+          {@html intro}
+        </div>
+      {/if}
+
+      {#if text}
+        <div class="text">
+          {@html text}
+        </div>
+      {/if}
+
+      {#if formLinkUrl && formLinkText}
+        <a href={formLinkUrl} class="button button--primary self-start">
+          {formLinkText}
+        </a>
+      {/if}
+    </article>
+  </div>
+</Story>
+
+<style lang="scss">
+  @use 'src/scss/app';
+</style>
