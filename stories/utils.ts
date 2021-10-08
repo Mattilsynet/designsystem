b77ff95ab095f11f3713090b99d43e4f001183ce
Lisa <halvorsen.lisa@gmail.com>
2021-10-08 10:01:09 +0200

    Add aria-describedby to file upload (#47)
    Relates to MT-323

--- stories/utils.ts
+++ stories/utils.ts
@@ -1,3 +1,5 @@
+import {UIError} from '../src/ts/types';
+
 export function wrapInShadowDom(el: HTMLElement, hideCss: false) {
   if (hideCss) {
     const shadow = el.attachShadow({
@@ -11,6 +13,10 @@ export function wrapInShadowDom(el: HTMLElement, hideCss: false) {
 export function splitIntoParagraphs(str: string) {
   return str
     .split('\n')
-    .map((str) => `<p>${str}</p>`)
-    .join("");
+    .map(str => `<p>${str}</p>`)
+    .join('');
+}
+
+export function createInputAriaDescribedby(name: string, error: UIError | undefined): string {
+  return error ? `${name}-hint ${name}-error` : `${name}-hint`;
 }
