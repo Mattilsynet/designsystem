56902f4e4cc44924bab176482605d97ebf400ca9
Lisa <halvorsen.lisa@gmail.com>
2021-10-11 16:11:00 +0200

    Refac
    - to make more customizable
    
    Relates to MT-354

--- src/svelte/ButtonSpinner.svelte
+++ src/svelte/ButtonSpinner.svelte
@@ -1,30 +1,29 @@
 <script lang="ts">
-  export let formInProgressAriaLabel = ''
-  export let color: 'primary' | 'secondary' = 'primary'
-  export let spinnerPlacement: 'start' | 'end' = 'end'
+  export let formInProgressAriaLabel = '';
+  export let spinnerPlacement: 'start' | 'end' = 'end';
+  export let btnClassNames = '';
 
-  let inProgress = false
-  let formInProgressLabel = ''
-  let colorClass = color === 'primary' ? 'button--primary' : 'button--secondary'
+  let inProgress = false;
+  let formInProgressLabel = '';
 
-  function setInProgress(e){
-    if(inProgress){
-      e.preventDefault()
+  function setInProgress(e) {
+    if (inProgress) {
+      e.preventDefault();
     } else {
-      inProgress = true
-      e.target.blur()
-      formInProgressLabel = formInProgressAriaLabel
-      e.target.focus()
+      inProgress = true;
+      e.target.blur();
+      formInProgressLabel = formInProgressAriaLabel;
+      e.target.focus();
     }
   }
 </script>
 
-<button on:click={setInProgress} class="button button--spinner {colorClass}" aria-label={formInProgressLabel} >
+<button on:click={setInProgress} class="button button--spinner {btnClassNames}" aria-label={formInProgressLabel}>
   {#if inProgress && spinnerPlacement === 'start'}
-    <span class="spinner spinner--start"/>
+    <span class="spinner spinner--start" />
   {/if}
-  <slot/>
+  <slot />
   {#if inProgress && spinnerPlacement === 'end'}
-    <span class="spinner spinner--end"/>
+    <span class="spinner spinner--end" />
   {/if}
 </button>
