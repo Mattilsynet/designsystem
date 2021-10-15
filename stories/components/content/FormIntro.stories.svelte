7677b43716738b4eaa78df7db43970cde75266ca
Lisa <halvorsen.lisa@gmail.com>
2021-10-15 11:22:54 +0200

    Update stories
    - add story for form-intro and form-intro-module
    
    Relates to MT-22

--- stories/components/content/FormIntro.stories.svelte
+++ stories/components/content/FormIntro.stories.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
   import {Meta, Story} from '@storybook/addon-svelte-csf';
-  import CardArticle from '../CardArticle.svelte';
 </script>
 
 <Meta
-  title="Innhold/Skjemaforside (modul)"
+  title="Innhold/Skjemaforside"
   args={{
     title: 'Varsle om piggtrådgjerder',
-    intro: '',
+    intro: `<p>Dette er introen</p>`,
     text: `<p>Du bør varsle Mattilsynet hvis du oppdager</p>
 <ul>
  <li>at noen setter opp nye piggtrådgjerder</li>
  <li>eldre piggtrådgjerder som utgjør en risiko for at dyr blir skadet</li>
  </ul>`,
+    moreText: `<h2>Du bør varsle</h2>
+ <p>At noen setter opp nye piggtrådgjerder</p>
+ <p>Eldre piggtrådgjerder som utgjør en risiko for at dyr blir skadet</p>
+ `,
     formLinkUrl: 'http://',
     formLinkText: 'Lenke til skjema',
     disableCss: false
@@ -25,7 +28,31 @@
 
 <Story name="Normal" let:title let:intro let:text let:moreText let:formLinkUrl let:formLinkText>
   <div class="container">
-    <CardArticle type="form-intro" linkText={formLinkText} linkUrl={formLinkUrl} {intro} {title} {text} id={title} />
+    <div class="layout-flex-col form-intro">
+      <article class="content">
+        <h1>{title}</h1>
+
+        {#if intro}
+          <div class="intro">
+            {@html intro}
+          </div>
+        {/if}
+
+        {#if text}
+          {@html text}
+        {/if}
+
+        {#if moreText}
+          {@html moreText}
+        {/if}
+
+        {#if formLinkUrl && formLinkText}
+          <a href={formLinkUrl} class="button button--primary self-start">
+            {formLinkText}
+          </a>
+        {/if}
+      </article>
+    </div>
   </div>
 </Story>
 
