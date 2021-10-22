5c9b7672c9534aad8fc9200d7f3c8ff1cd26ca83
Lisa <halvorsen.lisa@gmail.com>
2021-10-22 13:05:52 +0200

    Adjust text size on counter text (#54)
    Relates to MT-168

--- src/ts/count-characters.ts
+++ src/ts/count-characters.ts
@@ -3,6 +3,7 @@ import type {Action} from './types';
 export const countCharacters: Action<HTMLInputElement | HTMLTextAreaElement> = node => {
   const counterEl = document.createElement('div');
   counterEl.classList.add('self-end');
+  counterEl.classList.add('text-small');
 
   function updateCounter() {
     counterEl.innerText = `${node.maxLength - node.value.length} tegn igjen`;
