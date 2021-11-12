b6488ae541956a81af33f0d5950253a15f2433d8
Tom Arild Jakobsen <tajakobsen@gmail.com>
2021-11-12 08:37:16 +0100

    Add Chapter Menu component (#66)
    * Add Chapter Menu component
    
    Relates to: MT-490
    
    * Update scss after review
    
    Relates to: MT-490
    
    * Update ChapterMenu with more types
    
    Relates to: MT-490

---
+++ src/svelte/ChapterMenu.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import {createEventDispatcher} from 'svelte';
+  import {toKebabCase} from '../ts/utils';
+  export let chapters: Array<{
+    heading: string;
+    index: number;
+  }>;
+  export let currentChapterNumber = 0;
+  export let basePath: string;
+  export let menuTitle: string;
+
+  interface ChapterChangeDetails {
+    index: number;
+  }
+
+  const dispatch = createEventDispatcher<{chapterChange: ChapterChangeDetails}>();
+</script>
+
+<nav class="chapter-menu" aria-labelledby="chapter-menu-title">
+  <h3 id="chapter-menu-title" class="h4">{menuTitle}</h3>
+
+  <ol class="list-unstyled">
+    {#each chapters as chapter}
+      <li>
+        <a
+          on:click|preventDefault={dispatch('chapterChange', {index: chapter.index})}
+          href="{basePath}?kapittel={chapter.index}-{toKebabCase(chapter.heading)}"
+          aria-current={chapter.index === currentChapterNumber ? 'page' : undefined}
+        >
+          {chapter.heading}
+        </a>
+      </li>
+    {/each}
+  </ol>
+</nav>
