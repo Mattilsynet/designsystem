9a4613c44ee5a8646d2313f41dfdae8b9b424d9e
Lisa <halvorsen.lisa@gmail.com>
2021-11-19 11:05:45 +0100

    Add compoenent for label
    Relates to MT-292

---
+++ src/svelte/form/Label.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let htmlFor: string;
+  export let textOptional: string | undefined = 'valgfitt felt';
+  export let isRequired: boolean;
+</script>
+
+<label class="form-label" for={htmlFor}>
+  <slot />
+  {#if !isRequired}
+    <span class="text-small">({textOptional})</span>
+  {/if}
+</label>
