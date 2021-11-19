51a98a1c79b1a74187b0ed2e106a4c6f5100d84c
Lisa <halvorsen.lisa@gmail.com>
2021-11-19 11:15:08 +0100

    Add input error message from web-project
    - add exported var for hiddenErrorText to be able to translate
    
    Relates to MT-292

---
+++ src/svelte/form/InputErrorMessage.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let key: string;
+  export let message: string;
+  export let hiddenErrorText: string = 'Feilmelding';
+</script>
+
+<span id={`${key}-error`} class="form-error">
+  <span class="inclusively-hidden">{hiddenErrorText}:</span>
+  {message}
+</span>
