b63eefebe402d48ff230c185043d7dac143efbe5
Lisa <halvorsen.lisa@gmail.com>
2021-11-22 13:54:06 +0100

    Update spacing story
    - to show different elements in grid
    
    Relates to MT-338

--- stories/visualStyle/Spacing.stories.svelte
+++ stories/visualStyle/Spacing.stories.svelte
@@ -2,6 +2,8 @@
   import {Meta, Story} from '@storybook/addon-svelte-csf';
   import Breadcrumbs from '../../src/svelte/Breadcrumbs.svelte';
   import Disclosure from '../../src/svelte/Disclosure.svelte';
+
+  import imageFile from '../../.static/testbilde.jpg';
 </script>
 
 <Meta
@@ -72,74 +74,97 @@
 >
   <body>
     <header class="header header--regular" />
-    <main class="container content">
+    <div class="container content layout-grid layout-grid--column-12">
       <Breadcrumbs {breadcrumbs} />
-      <div class="layout-flex-col">
-        <div class="layout-flex-col">
-          <section class="content">
-            <h1>Varsle Mattilsynet</h1>
-          </section>
-        </div>
-        <div class="layout-flex-col">
-          <div class="layout-grid layout-grid--column-3">
-            {#each transportCards as card}
-              <a href={card.href} class="button button--primary transport-card">{card.text}</a>
-            {/each}
+      <main id="main">
+        <div data-portal-region="main">
+          <div>
+            <section class="content">
+              <h1>Varsle Mattilsynet</h1>
+            </section>
           </div>
-        </div>
-        <div class="layout-flex-col">
-          <section class="content">
-            <h2>Nyttig å vite</h2>
-            <Disclosure title={'Kan jeg være anonym?'}>
+          <div class="card-grid">
+            <div class="layout-grid layout-grid--column-3">
+              {#each transportCards as card}
+                <a href={card.href} class="button button--primary transport-card">
+                  <svg width="61" height="61" viewBox="0 0 61 61" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M11.4382 30.5001C14.9475 30.5001 17.7923 27.6552 17.7923 24.1459C17.7923 20.6366 14.9475 17.7917 11.4382 17.7917C7.92884 17.7917 5.08398 20.6366 5.08398 24.1459C5.08398 27.6552 7.92884 30.5001 11.4382 30.5001Z"
+                      fill="white"
+                    />
+                    <path
+                      d="M22.8757 20.3333C26.385 20.3333 29.2298 17.4885 29.2298 13.9792C29.2298 10.4699 26.385 7.625 22.8757 7.625C19.3663 7.625 16.5215 10.4699 16.5215 13.9792C16.5215 17.4885 19.3663 20.3333 22.8757 20.3333Z"
+                      fill="white"
+                    />
+                    <path
+                      d="M38.1257 20.3333C41.635 20.3333 44.4798 17.4885 44.4798 13.9792C44.4798 10.4699 41.635 7.625 38.1257 7.625C34.6163 7.625 31.7715 10.4699 31.7715 13.9792C31.7715 17.4885 34.6163 20.3333 38.1257 20.3333Z"
+                      fill="white"
+                    />
+                    <path
+                      d="M49.5632 30.5001C53.0725 30.5001 55.9173 27.6552 55.9173 24.1459C55.9173 20.6366 53.0725 17.7917 49.5632 17.7917C46.0538 17.7917 43.209 20.6366 43.209 24.1459C43.209 27.6552 46.0538 30.5001 49.5632 30.5001Z"
+                      fill="white"
+                    />
+                    <path
+                      d="M44.0727 37.7692C41.8615 35.1767 40.006 32.9654 37.7694 30.3729C36.6002 29.0004 35.1006 27.6279 33.3215 27.0179C33.0419 26.9163 32.7623 26.84 32.4827 26.7892C31.8473 26.6875 31.1611 26.6875 30.5002 26.6875C29.8394 26.6875 29.1531 26.6875 28.4923 26.8146C28.2127 26.8654 27.9331 26.9417 27.6536 27.0433C25.8744 27.6533 24.4002 29.0258 23.2056 30.3983C20.9944 32.9908 19.139 35.2021 16.9023 37.7946C13.5727 41.1242 9.48064 44.8096 10.2431 49.9692C10.9802 52.5617 12.8356 55.1287 16.1652 55.8658C18.0206 56.2471 23.9427 54.7475 30.246 54.7475H30.7036C37.0069 54.7475 42.929 56.2217 44.7844 55.8658C48.114 55.1287 49.9694 52.5362 50.7065 49.9692C51.4944 44.7842 47.4023 41.0987 44.0727 37.7692Z"
+                      fill="white"
+                    />
+                  </svg>
+                  <span class="text">
+                    {card.text}
+                  </span>
+                </a>
+              {/each}
+            </div>
+          </div>
+          <div>
+            <section class="content">
+              <h2>Nyttig å vite</h2>
+              <Disclosure title={'Kan jeg være anonym?'}>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                  industry's standard dummy text ever since the 1500s
+                </p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <ul>
+                  <li>Lorem Ipsum is simply dummy text of the dummy text of the dummy text of the</li>
+                  <li>Lorem Ipsum is simply dummy</li>
+                  <li>Lorem Ipsum is simply dummy text of the dummy text of the</li>
+                  <li>
+                    Lorem Ipsum is simply dummy text of the dummy text of the
+                    <ul>
+                      <li>Lorem Ipsum is simply dummy</li>
+                      <li>Lorem Ipsum is simply dummy</li>
+                    </ul>
+                  </li>
+                </ul>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              </Disclosure>
+            </section>
+          </div>
+          <div class="article-view">
+            <article class="article-page">
+              <p>
+                <strong>Takk for at du meldte fra til oss!</strong>
+              </p>
+              <figure>
+                <img src={imageFile} alt="Alternativ bildetekst" />
+                <figcaption>Bildetekst</figcaption>
+              </figure>
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
                 industry's standard dummy text ever since the 1500s
               </p>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-              <ul>
-                <li>Lorem Ipsum is simply dummy text of the dummy text of the dummy text of the</li>
-                <li>Lorem Ipsum is simply dummy</li>
-                <li>Lorem Ipsum is simply dummy text of the dummy text of the</li>
-                <li>
-                  Lorem Ipsum is simply dummy text of the dummy text of the
-                  <ul>
-                    <li>Lorem Ipsum is simply dummy</li>
-                    <li>Lorem Ipsum is simply dummy</li>
-                  </ul>
-                </li>
-              </ul>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-            </Disclosure>
-          </section>
+            </article>
+          </div>
         </div>
-        <div class="layout-flex-col">
-          <section class="content">
-            <p>
-              <strong>Takk for at du meldte fra til oss!</strong>
-            </p>
-            <ul>
-              <li>
-                Melder du&nbsp;anonymt har vi ikke noen måte å kontakte deg på for å følge opp saken med deg direkte.
-              </li>
-              <li>
-                Vi er likevel glad for at du meldte fra til oss. Det du melder vil kunne&nbsp;bedre forholdene for
-                dyrene.
-              </li>
-              <li>
-                Bekymringsmeldingen sendes til teamet som vurderer slike meldinger. Teamet avgjør om og hvordan saken
-                følges opp. Dette avhenger av hvor informativ og alvorlig bekymringsmeldingen&nbsp;er.
-              </li>
-              <li>
-                Vi behandler mange saker, og i mange av sakene har vi taushetsplikt. Du vil derfor ikke få noen
-                informasjon om den videre saksgangen og hva vi gjør med saken.
-              </li>
-            </ul>
-            <p><strong>Ditt referansenummer i saken er: 109920</strong></p>
-            <p>Hilsen oss i Mattilsynet</p>
-          </section>
-        </div>
-      </div>
-    </main>
+      </main>
+    </div>
     <footer class="footer" />
   </body>
 </Story>
+
+<style>
+  img {
+    width: 100%;
+  }
+</style>
