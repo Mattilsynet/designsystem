d6d2534819575ad008d4e171bc8e5be902c145f8
Øyvind Dahl <37140327+itemodahl@users.noreply.github.com>
2021-11-26 10:12:31 +0100

    Add Dropdown component (#75)
    Relates to MT-447
    
    Co-authored-by: Tom Arild Jakobsen <tajakobsen@gmail.com>

---
+++ src/svelte/Dropdown.svelte
@@ -0,0 +1,79 @@
+<script context="module">
+  let counter = 0;
+</script>
+
+<script lang="ts">
+  import {assign, createMachine} from 'xstate';
+  import {useMachine} from '@xstate/svelte';
+  import {onMount} from 'svelte';
+  import {slide} from 'svelte/transition';
+
+  export let title = '';
+  const bodyId = `ui-dropdown-${counter++}`;
+  export let loadJs = false;
+  export let titleId = `${bodyId}-title`;
+
+  interface DropdownContext {
+    isFirstRenderFinished: boolean;
+  }
+
+  type DropdownEvent = {type: 'MOUNT'} | {type: 'TOGGLE'};
+
+  type DropdownState =
+    | {value: 'serverRendered'; context: DropdownContext}
+    | {value: 'open'; context: DropdownContext}
+    | {value: 'closed'; context: DropdownContext};
+
+  const DropdownMachine = createMachine<DropdownContext, DropdownEvent, DropdownState>({
+    id: 'dropdown',
+    initial: 'serverRendered',
+    context: {
+      isFirstRenderFinished: false
+    },
+    states: {
+      serverRendered: {
+        on: {MOUNT: 'closed'}
+      },
+      closed: {
+        exit: assign({isFirstRenderFinished: true}),
+        on: {TOGGLE: 'open'}
+      },
+      open: {
+        on: {TOGGLE: 'closed'}
+      }
+    }
+  });
+
+  const {state, send} = useMachine(DropdownMachine);
+  $: isOpen = $state.value === 'open';
+  $: onServer = $state.value === 'serverRendered';
+
+  if (loadJs) {
+    onMount(() => send('MOUNT'));
+  }
+</script>
+
+<div aria-label={title} class="dropdown" class:visible={isOpen || onServer}>
+  {#if onServer}
+    <div class="dropdown-title">{title}</div>
+  {:else}
+    <button
+      id={titleId}
+      class="button button--link"
+      aria-haspopup="true"
+      aria-expanded={isOpen}
+      aria-controls={bodyId}
+      on:click={() => send('TOGGLE')}
+    >
+      {title}
+    </button>
+  {/if}
+
+  {#key isOpen}
+    <div class="dropdown-content" id={bodyId}>
+      <div in:slide={{duration: 300}}>
+        <slot />
+      </div>
+    </div>
+  {/key}
+</div>
---
+++ stories/Dropdown.stories.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+  import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+  import Dropdown from '../src/svelte/Dropdown.svelte';
+
+  const configs = [
+    {
+      buttonLabel: 'Språk/language',
+      disableJs: false,
+      items: [
+        {
+          title: 'Vis denne siden på nynorsk',
+          url: '#'
+        },
+        {
+          title: 'Show this page in English',
+          url: '#"'
+        }
+      ]
+    },
+    {
+      buttonLabel: 'En annen tekst',
+      disableJs: false,
+      items: [
+        {
+          title: 'En kort lenke',
+          url: '#'
+        },
+        {
+          title: 'En lengere lenke, med komma',
+          url: '#"'
+        }
+      ]
+    }
+  ];
+</script>
+
+<Meta
+  parameters={{xstate: true, inspectUrl: 'https://stately.ai/viz?inspect'}}
+  title="Components/Dropdown"
+  argTypes={{
+    buttonLabel: {control: 'text'},
+    disableJs: {control: 'boolean'}
+  }}
+/>
+
+<Template let:args>
+  <section class="preview-wrapper">
+    <Dropdown title={args.buttonLabel} loadJs={!args.disableJs} let:titleId>
+      <ol class="alt-language" aria-labelledby={titleId}>
+        {#each args.items as item}
+          <li>
+            <a href={item.url} class="forward-arrow-small">{item.title}</a>
+          </li>
+        {/each}
+      </ol>
+    </Dropdown>
+  </section>
+</Template>
+
+<Story name="Normal" args={configs[0]} />
+
+<Story name="Multiple" args={configs} let:args>
+  {#each configs as args, i}
+    <section class={`preview-wrapper`}>
+      <Dropdown title={args.buttonLabel} loadJs={!args.disableJs} let:titleId>
+        <ol class="alt-language" aria-labelledby={titleId}>
+          {#each args.items as item}
+            <li>
+              <a href={item.url} class="forward-arrow-small">{item.title}</a>
+            </li>
+          {/each}
+        </ol>
+      </Dropdown>
+    </section>
+  {/each}
+</Story>
+
+<style>
+  .preview-wrapper {
+    text-align: right;
+    margin: var(--spacer-small) 0;
+  }
+  .alt-language {
+    text-align: left;
+    list-style: none;
+    padding: 0;
+    color: white;
+  }
+</style>
