7d283d6bb7347f05493eff0a9edce5e4920dca00
Lisa <halvorsen.lisa@gmail.com>
2021-12-14 16:35:28 +0100

    Add highlighted content link component
    MT-650

---
+++ src/svelte/HighlightedContentLink.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import {displayDataTime} from '../ts/utils';
+
+  let url = '';
+  let className = '';
+  export {url as href};
+  export {className as class};
+  export let image: {src: string; alt: string | undefined} | undefined = undefined;
+  export let lang = 'nb-NO';
+  export let date: string | undefined;
+  export let heading: string | undefined;
+  export let headingClass: string = '';
+
+  $: displayDate = displayDataTime(lang, date);
+</script>
+
+<a href={url} class="hightlighted-content {className}">
+  {#if image}
+    <img src={image.src} alt={image.alt} />
+  {/if}
+  <div class="layout-flex layout-flex-col highlighted" style="gap:0">
+    {#if date}
+      <time class="text-small" datetime={date}>{displayDate}</time>
+    {/if}
+    {#if heading}
+      <h3 class={`heading ${headingClass}`}>
+        {heading}
+      </h3>
+    {/if}
+    <slot />
+  </div>
+</a>
---
+++ stories/components/HighlightedContentLink.stories.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import {Meta, Story} from '@storybook/addon-svelte-csf';
+  import HighlightedContentLink from '../../src/svelte/HighlightedContentLink.svelte';
+  import imageFile from '../../.static/testbilde.jpg';
+</script>
+
+<Meta
+  title="Components/Fremhevet innhold"
+  args={{
+    text: `<p>Haster det? Er det fare for at dyr dør eller blir påført store belastninger. Ring mattilsynet på tlf. <a href="tel:22400000">22 40 00 00</a> eller politiet <a href="tel:02800">02800</a></p>`,
+
+    heading: `<h3 class="heading">Tilsyn med småfe</h3>`,
+    date: '2021-06-24T11:32:22Z',
+    disableJs: false,
+    disableCss: false
+  }}
+  argTypes={{
+    text: {control: 'string'},
+    heading: {control: 'string'},
+    date: {control: 'string'},
+    disableJs: {control: 'boolean'},
+    disableCss: {control: 'boolean'}
+  }}
+/>
+
+<Story name="Normal" let:text let:disableCss let:heading let:date>
+  <main class="container layout-flex-col">
+    <h1>Fremhevet innhold</h1>
+    <section class="layout-flex-col">
+      <HighlightedContentLink
+        href="https://www.mattilsyent.no"
+        image={{src: imageFile, alt: 'Her er alt tekst til bildet'}}
+        headingClass="h3"
+        {date}
+        heading="Dette er tittelen"
+      >
+        {@html text}
+      </HighlightedContentLink>
+    </section>
+  </main>
+</Story>
+
+<style lang="scss">
+  main {
+    --gap: var(--spacer-large);
+  }
+  section {
+    --gap: var(--spacer-xx-small);
+  }
+</style>
