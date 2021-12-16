b61cebc75c2122a1f5d7dc8e6171b3b35f5e9100
Lisa <halvorsen.lisa@gmail.com>
2021-12-16 16:41:57 +0100

    Update highlighted content
    - remove date. not needed
    - add check for src for image
    
    MT-650

--- src/svelte/HighlightedContentLink.svelte
+++ src/svelte/HighlightedContentLink.svelte
@@ -1,27 +1,19 @@
 <script lang="ts">
-  import type {displayDataTime} from '../ts/utils';
-
   let url = '';
   let className = '';
   export {url as href};
   export {className as class};
   export let image: {src: string; alt: string | undefined} | undefined = undefined;
   export let lang = 'nb-NO';
-  export let date: string | undefined;
   export let heading: string | undefined;
   export let headingClass: string = '';
-
-  $: displayDate = displayDataTime(lang, date);
 </script>
 
 <a href={url} class="hightlighted-content {className}">
-  {#if image}
+  {#if image.src}
     <img src={image.src} alt={image.alt} />
   {/if}
   <div class="layout-flex layout-flex-col highlighted-text" style="gap:0">
-    {#if date}
-      <time class="text-small" datetime={date}>{displayDate}</time>
-    {/if}
     {#if heading}
       <h3 class={`heading ${headingClass}`}>
         {heading}
