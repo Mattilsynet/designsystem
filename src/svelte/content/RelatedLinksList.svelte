b6d39600a52303ebda91868dc450670b90a5b05f
Lisa <halvorsen.lisa@gmail.com>
2022-01-07 15:40:25 +0100

    MT-663: Add forward arrow end link (#177)
    * Add forward arrow end link
    - new link style for custom link list
    
    * Add Related links list component
    - update styling for focus
    
    MT-663

---
+++ src/svelte/content/RelatedLinksList.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import {toKebabCase} from '../../ts/utils'
+
+  export let title: string | undefined
+  export let linkGroups: Array<{title: string; links: Array<{url: string; text: string}>}> = []
+  export let leftColClass = ''
+  export let rightColClass = ''
+</script>
+
+<div class="{leftColClass} layout-flex-col" style="gap: 0" data-testid="left-col">
+  <h2>{title ? title : ''}</h2>
+</div>
+<div class="{rightColClass} layout-flex-col link-group" style="gap: 0" data-testid="right-col">
+  {#each linkGroups as group}
+    <h3 id={toKebabCase(group.title)}>{group.title}</h3>
+    <ul class="list-unstyled" aria-labelledby={toKebabCase(group.title)}>
+      {#each group.links as link}
+        <li>
+          <a href={link.url} class="forward-arrow-end-link">{link.text}</a>
+        </li>
+      {/each}
+    </ul>
+  {/each}
+</div>
+
+<style lang="scss">
+  .link-group > :first-child {
+    margin-top: var(--spacer-xx-small);
+  }
+  ul + h3 {
+    margin-top: var(--spacer-medium);
+  }
+</style>
---
+++ stories/components/content/RelatedLinksList.stories.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import {Meta, Story} from '@storybook/addon-svelte-csf'
+  import Status from '../../../src/svelte/Status.svelte'
+  import RelatedLinksList from '../../../src/svelte/content/RelatedLinksList.svelte'
+</script>
+
+<Meta
+  title="Innhold/Relatert innhold lenke liste"
+  args={{
+    title: 'Hovedoverskrift',
+    linkGroups: [
+      {
+        title: 'Overskrift 1',
+        links: [
+          {url: 'https://www.mattilsynet.no', text: 'mattilsynet.no'},
+          {url: 'https://www.mattilsynet.no', text: 'Annen link'}
+        ]
+      },
+      {
+        title: 'Overskrift 2',
+        links: [
+          {url: 'https://www.mattilsynet.no', text: 'mattilsynet.no'},
+          {url: 'https://www.mattilsynet.no', text: 'Annen link'}
+        ]
+      }
+    ],
+    disableCss: false
+  }}
+  argTypes={{
+    title: {control: 'text'},
+    linkGroups: {control: 'array'},
+    disableCss: {control: 'boolean'}
+  }} />
+
+<Story name="Normal" let:title let:linkGroups let:disableCss>
+  <div class="container layout-grid layout-grid--column-12">
+    <RelatedLinksList
+      {title}
+      {linkGroups}
+      leftColClass="col-1-span-5"
+      rightColClass="col-7-span-6" />
+  </div>
+</Story>
+
+<style lang="scss">
+  .margin-top {
+    margin-top: var(--spacer-large);
+  }
+</style>
