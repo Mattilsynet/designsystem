0c577672de0ea2233631ff6301691cb85989294e
Lisa <halvorsen.lisa@gmail.com>
2022-01-12 14:59:23 +0100

    Mt-697 Add text to updated date (#187)
    Mt-697

--- src/svelte/Published.svelte
+++ src/svelte/Published.svelte
@@ -1,25 +1,28 @@
 <script lang="ts">
-  import {displayDataTime} from '../ts/utils';
+  import {displayDataTime} from '../ts/utils'
 
-  export let publishFrom: string | undefined;
-  export let professionallyUpdated: string | undefined;
-  export let lang = 'nb-NO';
-  export let publishedText = 'Publisert';
-  export let lastPublishedText = 'Sist oppdatert';
+  export let publishFrom: string | undefined
+  export let professionallyUpdated: string | undefined
+  export let lang = 'nb-NO'
+  export let publishedText = 'Publisert'
+  export let lastPublishedText = 'Sist oppdatert'
 
-  $: publishFromLocalized = publishFrom ? displayDataTime(lang, publishFrom) : '';
-  $: lastUpdatedLocalized = professionallyUpdated ? displayDataTime(lang, professionallyUpdated) : '';
+  $: publishFromLocalized = publishFrom ? displayDataTime(lang, publishFrom) : ''
+  $: lastUpdatedLocalized = professionallyUpdated
+    ? displayDataTime(lang, professionallyUpdated)
+    : ''
 </script>
 
 <dl class="meta">
   {#if publishFrom}
     <dd>{publishedText}</dd>
-    <dt><time datetime={publishFrom}>{publishFromLocalized}</time></dt>
+    <dt><time datetime={publishFrom} data-testid="published-date">{publishFromLocalized}</time></dt>
   {/if}
   {#if professionallyUpdated}
     <dd>{lastPublishedText}</dd>
     <dt>
-      <time datetime={professionallyUpdated}>{lastUpdatedLocalized}</time>
+      <time datetime={professionallyUpdated} data-testid="updated-date"
+        >{lastUpdatedLocalized}</time>
     </dt>
   {/if}
 </dl>
