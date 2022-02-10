9d476a6967ff4d6c9b70b230c10f1f00a2c42c1f
milosz <miloszhygen@gmail.com>
2022-02-10 23:41:58 +0100

    Improvements to the SummaryDetail component
    - updated the title to `Components/Oppsummering og Detaljer`
    - made the content dynamic
    - added `argTypes`

--- stories/components/SummaryDetail.stories.svelte
+++ stories/components/SummaryDetail.stories.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
+  import {splitIntoParagraphs} from './../utils'
   import {Meta, Story} from '@storybook/addon-svelte-csf';
   import SummaryDetail from '../../src/svelte/SummaryDetail.svelte';
 </script>
 
 <Meta
-  title="Components/Summary Detail"
+  title="Components/Oppsummering og Detaljer"
   args={{
     title:'Eksempel på oppsummering og detaljer',
+    content: splitIntoParagraphs(
+      `For at meldingen skal kunne følges opp, må den inneholde opplysninger som tyder på alvorlig brudd på dyrevelferdsregelverket. For eksempel mishandling av dyr, alvorlig svikt i levemiljø, tilsyn og stell, syke og skadde dyr som ikke følges opp.\n Det er viktig med konkrete beskrivelser. Vage og generelle beskrivelser basert på antagelser er ikke ønskelig.\n Skriv gjerne\n Hvor skjedde det?\n
+      Her er andre punkt i listen\n Så er det noe tekst på bunnen av summary`)
+  }}
+  argTypes={{
+    title: {control: 'text'},
+    content: {control: 'text'},
   }}
 />
 
@@ -14,21 +22,7 @@
   <main class="container layout-flex-col">
     <h1>Oppsummering og detaljer</h1>
     <SummaryDetail title={args.title}>
-      <p>
-        For at meldingen skal kunne følges opp, må den inneholde opplysninger som tyder på alvorlig brudd på
-        dyrevelferdsregelverket. For eksempel mishandling av dyr, alvorlig svikt i levemiljø, tilsyn og stell, syke
-        og skadde dyr som ikke følges opp.
-      </p>
-      <p>
-        Det er viktig med konkrete beskrivelser. Vage og generelle beskrivelser basert på antagelser er ikke
-        ønskelig.
-      </p>
-      <p>Skriv gjerne</p>
-      <ul>
-        <li>Hvor skjedde det?</li>
-        <li>Her er andre punkt i listen</li>
-      </ul>
-      <p>Så er det noe tekst på bunnen av summary</p>
+      {@html args.content}
     </SummaryDetail>
   </main>
 </Story>
