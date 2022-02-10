c08be257fb6ed2b3ddc5876c0810b84d895ccc84
milosz <miloszhygen@gmail.com>
2022-02-10 23:15:58 +0100

    Removed the wrapper class

--- src/svelte/SummaryDetail.svelte
+++ src/svelte/SummaryDetail.svelte
@@ -2,12 +2,9 @@
   export let title: string;
 </script>
 
-
-<div class="wrapper">
-  <details>
-    <summary>{title}</summary>
-    <div class="summary-wrapper">
-      <slot />
-    </div>
-  </details>
-</div>
+<details>
+  <summary>{title}</summary>
+  <div class="summary-wrapper">
+    <slot />
+  </div>
+</details>
