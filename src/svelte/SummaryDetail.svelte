042965c28ceaf636d85f5ec52bcaa39577dd3c7b
milosz <miloszhygen@gmail.com>
2022-02-10 12:31:30 +0100

    Created a SummaryDetail component

---
+++ src/svelte/SummaryDetail.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+
+<div class="wrapper">
+  <details>
+    <summary>{title}</summary>
+    <div class="summary-wrapper">
+      <slot />
+    </div>
+  </details>
+</div>
--- stories/components/SummaryDetail.stories.svelte
+++ stories/components/SummaryDetail.stories.svelte
@@ -1,45 +1,34 @@
 <script lang="ts">
-  import {Meta, Story} from '@storybook/addon-svelte-csf'
-  import FileUploadButton from '../../src/svelte/FileUploadButton.svelte'
+  import {Meta, Story} from '@storybook/addon-svelte-csf';
+  import SummaryDetail from '../../src/svelte/SummaryDetail.svelte';
 </script>
 
 <Meta
   title="Components/Summary Detail"
   args={{
-    disabled: false,
-    disableJs: false,
-    disableCss: false
+    title:'Eksempel på summary details',
   }}
-  argTypes={{
-    disabled: {control: 'boolean'},
-    disableJs: {control: 'boolean'},
-    disableCss: {control: 'boolean'}
-  }} />
+/>
 
-<Story name="Normal" let:primary let:primaryLong let:disableCss let:disabled let:secondary>
+<Story name="Normal" let:args >
   <main class="container layout-flex-col">
     <h1>Detail summary</h1>
-    <div class="wrapper">
-      <details>
-        <summary>Eksempel på summary details kompo&shy;nenten</summary>
-        <div class="summary-wrapper">
-          <p>
-            For at meldingen skal kunne følges opp, må den inneholde opplysninger som tyder på
-            alvorlig brudd på dyrevelferdsregelverket. For eksempel mishandling av dyr, alvorlig
-            svikt i levemiljø, tilsyn og stell, syke og skadde dyr som ikke følges opp.
-          </p>
-          <p>
-            Det er viktig med konkrete beskrivelser. Vage og generelle beskrivelser basert på
-            antagelser er ikke ønskelig.
-          </p>
-          <p>Skriv gjerne</p>
-          <ul>
-            <li>Hvor skjedde det?</li>
-            <li>Her er andre punkt i listen</li>
-          </ul>
-          <p>Så er det noe tekst på bunnen av summary</p>
-        </div>
-      </details>
-    </div>
+    <SummaryDetail title={args.title}>
+      <p>
+        For at meldingen skal kunne følges opp, må den inneholde opplysninger som tyder på alvorlig brudd på
+        dyrevelferdsregelverket. For eksempel mishandling av dyr, alvorlig svikt i levemiljø, tilsyn og stell, syke
+        og skadde dyr som ikke følges opp.
+      </p>
+      <p>
+        Det er viktig med konkrete beskrivelser. Vage og generelle beskrivelser basert på antagelser er ikke
+        ønskelig.
+      </p>
+      <p>Skriv gjerne</p>
+      <ul>
+        <li>Hvor skjedde det?</li>
+        <li>Her er andre punkt i listen</li>
+      </ul>
+      <p>Så er det noe tekst på bunnen av summary</p>
+    </SummaryDetail>
   </main>
 </Story>
