d9755ec690c713554a183707f3bd2cd549d28d5e
Lisa <halvorsen.lisa@gmail.com>
2022-02-14 15:39:16 +0100

    Update test with six decimals
    MT-832

--- src/svelte/Published.test.ts
+++ src/svelte/Published.test.ts
@@ -8,13 +8,13 @@ import Published from './Published.svelte'
 describe('Publised', () => {
   test('Renders published when published and professionallyUpdated is same', () => {
     const componentOptions = {
-      publishFrom: '2021-06-24T11:40:02.889Z',
-      professionallyUpdated: '2021-06-24T11:40:02.889Z'
+      publishFrom: '2022-02-01T13:10:52.905451Z',
+      professionallyUpdated: '2022-02-01T13:10:52.905451Z'
     }
     const {getByText, queryByText} = render(Published, componentOptions)
     expect(getByText('Publisert')).toBeInTheDocument()
     expect(queryByText('Faglig oppdatert')).not.toBeInTheDocument()
-    expect(getByText('24.06.2021')).toBeInTheDocument()
+    expect(getByText('01.02.2022')).toBeInTheDocument()
   })
 
   test('Renders profesionally updated when professionallyUpdated after published', () => {
