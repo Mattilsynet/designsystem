bac7de4b7fb1dd7089e2b2b8f6ae0db73e11cb1e
Lisa <halvorsen.lisa@gmail.com>
2022-02-18 10:12:38 +0100

    Add update values on removeFile
    MT-895

--- src/svelte/form/FileUpload.svelte
+++ src/svelte/form/FileUpload.svelte
@@ -1,30 +1,33 @@
 <script lang="ts">
-  import FileUploadButton from '../FileUploadButton.svelte';
-  import InputError from './InputErrorMessage.svelte';
-  import type {ErrorDetail} from '../../ts/types';
-  import Label from './Label.svelte';
+  import FileUploadButton from '../FileUploadButton.svelte'
+  import InputError from './InputErrorMessage.svelte'
+  import type {ErrorDetail} from '../../ts/types'
+  import Label from './Label.svelte'
 
-  export let value;
-  export let name: string;
-  export let label: string;
-  export let buttonText: string;
-  export let fileInputName: string;
-  export let fileNameInputName: string;
-  export let helpText: string | undefined;
-  export let fileName: string | undefined;
-  export let error: ErrorDetail | undefined;
-  export let isRequired: boolean | undefined = undefined;
-  export let accept: string | undefined;
-  export let multiple: boolean | undefined;
-  export let textOptional: string | undefined;
-  export let hiddenErrorText: string | undefined;
+  export let value
+  export let name: string
+  export let label: string
+  export let buttonText: string
+  export let fileInputName: string
+  export let fileNameInputName: string
+  export let helpText: string | undefined
+  export let fileName: string | undefined
+  export let error: ErrorDetail | undefined
+  export let isRequired: boolean | undefined = undefined
+  export let accept: string | undefined
+  export let multiple: boolean | undefined
+  export let textOptional: string | undefined
+  export let hiddenErrorText: string | undefined
 
-  let uuidInput: HTMLInputElement;
-  let nameInput: HTMLInputElement;
+  let uuidInput: HTMLInputElement
+  let nameInput: HTMLInputElement
 
   function onRemoveFile() {
-    uuidInput.value = '';
-    nameInput.value = '';
+    uuidInput.value = ''
+    nameInput.value = ''
+    //update inputs
+    value = undefined
+    fileName = undefined
   }
 </script>
 
@@ -52,5 +55,4 @@
   {accept}
   {fileName}
   {isRequired}
-  on:removeFile={onRemoveFile}
-/>
+  on:removeFile={onRemoveFile} />
