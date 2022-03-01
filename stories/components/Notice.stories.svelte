48f84e2f99eee02e96be7f792ad9a96cc3ff7a45
Øyvind Dahl <37140327+itemodahl@users.noreply.github.com>
2022-03-01 12:51:33 +0100

    💄 MT-759: Make notice content smaller on smaller screens (#267)
    MT-759

--- stories/components/Notice.stories.svelte
+++ stories/components/Notice.stories.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
-  import {Meta, Story} from '@storybook/addon-svelte-csf';
-  import Notice from '../../src/svelte/Notice.svelte';
+  import {Meta, Story} from '@storybook/addon-svelte-csf'
+  import Notice from '../../src/svelte/Notice.svelte'
 </script>
 
 <Meta
   title="Components/Notice"
   args={{
     text: `<p>Haster det? Er det fare for at dyr dør eller blir påført store belastninger. Ring mattilsynet på tlf. <a href="tel:22400000">22 40 00 00</a> eller politiet <a href="tel:02800">02800</a></p>`,
-    text2: `<p>Haster det? Er det fare for at dyr dør eller blir påført store belastninger</p> <ul>
-            <li>Hvor skjedde det?</li>
-            <li>Her er andre punkt i listen</li>
-          </ul>
-<p>Ekstra paragraph</p>`,
+    text2: `<p><strong>Ring politiet på tlf. 02800 hvis du</strong></p>
+<ul >
+  <li >har kjørt på et dyr</li>
+  <li >har funnet et sykt, skadd eller hjelpeløst dyr</li>
+  <li >vil varsle om farlige eller ulovlige hunder</li>
+  <li >vil varsle om dyr som forstyrrer ro og orden</li>
+</ul>`,
     disabled: false,
     disableJs: false,
     disableCss: false
@@ -20,8 +22,7 @@
     disabled: {control: 'boolean'},
     disableJs: {control: 'boolean'},
     disableCss: {control: 'boolean'}
-  }}
-/>
+  }} />
 
 <Story name="Normal" let:text let:text2 let:disableCss>
   <main class="container layout-flex-col">
