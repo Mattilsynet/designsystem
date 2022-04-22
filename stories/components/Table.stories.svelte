b3333aaed4d7133d404ff99fed61553c8f009fa9
milosz <miloszhygen@gmail.com>
2022-04-22 12:15:25 +0200

    MT-476 Improved the test html for tables

--- stories/components/Table.stories.svelte
+++ stories/components/Table.stories.svelte
@@ -10,41 +10,42 @@
     statusTitle: `Status tittel`,
     text: `<p>Status tekst</p>
 <table role="table">
-<caption>Dette er tittlen til tabelleb</caption>
+<caption>Dette er tittlen til tabellen</caption>
 <thead>
 <tr role="row">
-<th role="columnheader" scope="col">Tittel 1</th>
-<th role="columnheader" scope="col">Tittel 2</th>
-<th role="columnheader" scope="col">Tittel 3</th>
-<th role="columnheader" scope="col">Tittel 4</th>
-<th role="columnheader" scope="col">Tittel 5</th>
-<th role="columnheader" scope="col">Tittel 6</th>
+<th role="columnheader" scope="col">Kolonne 1</th>
+<th role="columnheader" scope="col">Kolonne 2</th>
+<th role="columnheader" scope="col">Kolonne 3</th>
+<th role="columnheader" scope="col">Kolonne 4</th>
+<th role="columnheader" scope="col">Kolonne 5</th>
+<th role="columnheader" scope="col">Kolonne 6</th>
 </tr>
 </thead>
 <tbody>
 <tr role="row">
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 1:</span>en</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 2:</span>en asdf asdf asdf asdfsadf dsf asdfdsdaffdaf asdfasf</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 3:</span>en asf a dfasd adsffas a</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 4:</span>en</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 5:</span>en asdfasd fadsfasd fasdf asdf asdf asd fasd fasdf </td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 6:</span>en</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 1 tittel 1:</span>Rad 1</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 1 tittel 2:</span>Rad 1 - med beskrivende tekst</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 1 tittel 3:</span>Rad 1 - med mer beskrivende tekst</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 1 tittel 4:</span>Rad 1</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 1 tittel 5:</span>Rad 1 med en lang beskrivende og status tekst </td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 1 tittel 6:</span>Rad 1</td>
 </tr>
 <tr role="row">
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 1:</span>to</th>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 2:</span>to</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 3:</span>to asdfasdf asdf asdf asdffds adsf</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 4:</span>to asdf adsfads fasdf asdf asfd asdf asf asdf e4awfsdfvasd sfv er</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 5:</span>to</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 6:</span>toa sdfasdf aerf wasdf asdf RFAEGSDFGA SDFDAsdf gafd ss</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 2 tittel 1:</span>Rad 2</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 2 tittel 2:</span>Rad 2 - med beskrivende tekst</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 2 tittel 3:</span>Rad 2 - med mer beskrivende tekst</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 2 tittel 4:</span>Rad 2</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 2 tittel 5:</span>Rad 2 med en lang beskrivende og status tekst </td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 2 tittel 6:</span>Rad 2</td>
 </tr>
-<tr>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 1:</span>to</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 2:</span>to</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 3:</span>to asdfasdf asdf asdf asdffds adsf</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 4:</span>to asdf adsfads fasdf asdf asfd asdf asf asdf e4awfsdfvasd sfv er</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 5:</span>to</td>
-<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Tittel 6:</span>toa sdfasdf aerf wasdf asdf RFAEGSDFGA SDFDAsdf gafd ss</td>
+</tr>
+<tr role="row">
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 3 tittel 1:</span>Rad 3</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 3 tittel 2:</span>Rad 3 - med beskrivende tekst</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 3 tittel 3:</span>Rad 3 - med mer beskrivende tekst</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 3 tittel 4:</span>Rad 3</td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 3 tittel 5:</span>Rad 3 med en lang beskrivende og status tekst </td>
+<td role="cell"><span class="responsive-table__heading" aria-hidden="true">Rad 3 tittel 6:</span>Rad 3</td>
 </tr>
 </tbody>
 </table>`,
