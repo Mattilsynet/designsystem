210573b7ef208c3ee925fddb0afdce29e245b1f3
Lisa <halvorsen.lisa@gmail.com>
2022-04-29 08:16:29 +0200

    Add menuitems comp

---
+++ src/svelte/content/MenuItems.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+  import Disclosure from '../Disclosure.svelte'
+  interface MenuItem {
+    title: string
+    path: string
+    hasChildren: boolean
+    url: string
+    type: string
+    children: Array<MenuItem>
+    iconResource?: string
+  }
+
+  export let itemsLeft: Array<MenuItem> = []
+  export let itemsRight: Array<MenuItem> = []
+  export let itemsBottom: Array<MenuItem> = []
+  export let splitColLimit = 5
+  export let titleId
+</script>
+
+<nav class="layout-grid layout-grid--column-12 ">
+  <ol class="menu border col-1-span-8" aria-labelledby={titleId}>
+    {#each itemsLeft as item}
+      {#if item.children && item.children.length > 0}
+        <li>
+          <Disclosure
+            title={item.title}
+            theme="no-border"
+            class="on-primary"
+            icon={item.iconResource}>
+            <a href={item.url} class="forward-arrow-small on-primary">{item.title}</a>
+            <ol class="sub-menu {item.children.length > splitColLimit ? 'split-long' : ''}">
+              {#each item.children || [] as child}
+                <li class="">
+                  <a href={child.url} class="forward-arrow-small on-primary">{child.title}</a>
+                </li>
+              {/each}
+            </ol>
+          </Disclosure>
+        </li>
+      {:else}
+        <li class="">
+          <a
+            href={item.url}
+            class="no-underline layout-flex layout-flex--center-vertical layout-full-width on-primary p-xs"
+            style="gap: var(--spacer-xx-small)">
+            {#if item.iconResource}
+              {@html item.iconResource}
+            {/if}
+            {item.title}
+          </a>
+        </li>
+      {/if}
+    {/each}
+  </ol>
+  {#if itemsRight}
+    <ol class="menu border col-10-span-3">
+      {#each itemsRight as right}
+        <li>
+          <a
+            href={right.url}
+            class="no-underline layout-flex layout-flex--center-vertical layout-full-width on-primary p-xs forward-arrow-end">
+            {right.title}
+          </a>
+        </li>
+      {/each}
+    </ol>
+  {/if}
+  {#if itemsBottom}
+    <ol class="col-1-span-8 menu-row m-t-m p-lr-xxs">
+      {#each itemsBottom as bottom}
+        <li class="m-t-0">
+          <a
+            href={bottom.url}
+            class="layout-flex layout-flex--center-vertical on-primary"
+            style="gap: var(--spacer-xx-small)">
+            {#if bottom.iconResource}
+              {@html bottom.iconResource}
+            {/if}
+            {bottom.title}
+          </a>
+        </li>
+      {/each}
+    </ol>
+  {/if}
+</nav>
