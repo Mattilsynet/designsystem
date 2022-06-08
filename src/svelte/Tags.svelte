816983c4c6321a440dbadfed1a1187c5e0b2b656
Lisa Halvorsen <halvorsen.lisa@gmail.com>
2022-06-08 13:15:20 +0200

    ✨MT-799 tags component (#437)
    * Add tags component
    
    * ✨ MT-799 Add tags component
    
    * Remove extra span
    
    * Update variable name
    
    For readability
    
    * Update test

---
+++ src/svelte/Tags.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let tags: Array<{text: string; ariaLabel: string}> = []
+  export let isClosable = false
+
+  function handleClick(index: number) {
+    tags = tags.filter((tag, tagIndex) => {
+      return tagIndex !== index
+    })
+  }
+</script>
+
+<ul class="list-unstyled tags">
+  {#each tags as tag, i}
+    <li class={isClosable ? 'tag-list-item' : 'tag-list-item tag-text'}>
+      {#if isClosable}
+        <button
+          class="button button--icon button--small closable m-r-0 m-t-0"
+          on:click={() => {
+            handleClick(i)
+          }}
+          aria-label={tag.ariaLabel}>
+          {tag.text}
+        </button>
+      {:else}
+        {tag.text}
+      {/if}
+    </li>
+  {/each}
+</ul>
---
+++ stories/components/Tags.stories.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import {Meta, Story} from '@storybook/addon-svelte-csf'
+  import Tags from '../../src/svelte/Tags.svelte'
+  import {wrapInShadowDom} from '../utils'
+
+  let closableTags = [
+    {text: 'Sverige', ariaLabel: 'Fjern Sverige fra listen'},
+    {text: 'Norge', ariaLabel: 'Fjern Norge fra listen'},
+    {text: 'Finland', ariaLabel: 'Fjern Finland fra listen'}
+  ]
+
+  function reset() {
+    closableTags = [
+      {text: 'Sverige', ariaLabel: 'Fjern Sverige fra listen'},
+      {text: 'Norge', ariaLabel: 'Fjern Norge fra listen'},
+      {text: 'Finland', ariaLabel: 'Fjern Finland fra listen'}
+    ]
+  }
+</script>
+
+<Meta
+  title="Components/Tags"
+  args={{
+    tags: [{text: 'Hund'}, {text: 'Katt'}, {text: 'Ilder'}],
+    isClosable: true,
+    disableCss: false
+  }}
+  argTypes={{
+    tags: {control: 'array'},
+    isClosable: {control: 'boolean'},
+    disableCss: {control: 'boolean'}
+  }} />
+
+<Story name="Normal" let:args>
+  <div use:wrapInShadowDom={args.disableCss} />
+  <h1>Tags</h1>
+  <div class="tags-wrapper">
+    <Tags tags={args.tags} />
+    <hr />
+    <h2>Lukkbare tags</h2>
+    <Tags bind:tags={closableTags} isClosable={args.isClosable} />
+    <button class="button button--link" on:click={reset}>Gjenopprett tags</button>
+  </div>
+</Story>
+
+<style lang="scss">
+  .tags-wrapper,
+  h2,
+  button {
+    margin-top: 2rem;
+  }
+</style>
