f1ce396848205d6923d66bfae53c645b23fbd880
milosz <miloszhygen@gmail.com>
2022-06-13 12:53:57 +0200

    MT-1215 Create dialog box component

---
+++ src/svelte/DialogBox.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let closeable: boolean = true
+
+  export let dialogBoxClosed: boolean = false
+
+  function handleClose() {
+    dialogBoxClosed = true
+  }
+</script>
+
+<div class={dialogBoxClosed ? 'dialog-box--closed' : 'dialog-box'}>
+  {#if closeable}
+    <div class="dialog-box--actions">
+      <button class="button button--link" on:click={handleClose} aria-label="tag.ariaLabel" />
+    </div>
+  {/if}
+
+  <div class="dialog-box--content">
+    <slot />
+  </div>
+</div>
