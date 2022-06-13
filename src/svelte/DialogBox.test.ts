a15064bf4cc594f44d6088a6e2e76623f6c3ba00
milosz <miloszhygen@gmail.com>
2022-06-13 14:26:10 +0200

    MT-1215 Added tests

---
+++ src/svelte/DialogBox.test.ts
@@ -0,0 +1,30 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render } from '@testing-library/svelte'
+import DialogBox from './DialogBox.svelte'
+
+describe('DialogBox', () => {
+
+  test('Renders when dialogBoxClosed is false', () => {
+    const componentOptions = {
+      dialogBoxClosed: false,
+    }
+    const { getByTestId } = render(DialogBox, componentOptions)
+    const dialogBox = getByTestId('dialog-box')
+
+    expect(dialogBox.classList[0]).toEqual('dialog-box')
+  })
+
+  test('Is hidden when dialogBoxClosed is true', () => {
+    const componentOptions = {
+      dialogBoxClosed: true,
+    }
+    const { getByTestId } = render(DialogBox, componentOptions)
+    const dialogBox = getByTestId('dialog-box')
+
+    expect(dialogBox.classList[0]).toEqual('dialog-box--closed')
+  })
+
+})
