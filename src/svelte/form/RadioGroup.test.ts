85b8db4fd98c2f700ad5a5a869f6629cdd6a06e6
Lisa Halvorsen <halvorsen.lisa@gmail.com>
2022-06-27 06:46:55 +0200

    💄MT-1276 Add radio styled as buttons (#462)
    * 💄MT-1276 Add radio styled as buttons
    
    * Add responsive
    
    * refac
    
    * refac
    
    * Two alternatives
    
    * Refac
    
    * Remove error styling on button after talking to designer

--- src/svelte/form/RadioGroup.test.ts
+++ src/svelte/form/RadioGroup.test.ts
@@ -1,29 +1,29 @@
 /**
  * @jest-environment jsdom
  */
-import {render} from '@testing-library/svelte';
-import RadioGroup from './RadioGroup.svelte';
+import {fireEvent, render} from '@testing-library/svelte'
+import RadioGroup from './RadioGroup.svelte'
 
 describe('RadioGroup', () => {
-  const value = 'yes';
-  const name = 'name';
-  const yesLabel = 'Ja';
-  const helpText = 'This is the helptext';
+  const value = 'yes'
+  const name = 'name'
+  const yesLabel = 'Ja'
+  const helpText = 'This is the helptext'
   const options = [
     {text: 'Ja', value: 'yes'},
     {text: 'Nei', value: 'no'}
-  ];
+  ]
   const properties = {
     validationRequired: {
       errorMessage: 'Error'
     }
-  };
+  }
   const error = {
     key: name,
     message: 'This is the error message'
-  };
+  }
   test('Renders', () => {
-    const { getByLabelText, getByDisplayValue, getByText, getByRole } = render(RadioGroup, {
+    const {getByLabelText, getByDisplayValue, getByText, getByRole} = render(RadioGroup, {
       value,
       error,
       name,
@@ -31,19 +31,52 @@ describe('RadioGroup', () => {
       helpText,
       options,
       isRequired: !!properties.validationRequired
-    });
-    expect(getByText(helpText)).toBeInTheDocument();
-    expect(getByLabelText(options[0].text)).toBeInTheDocument();
-    expect(getByLabelText(options[1].text)).toBeInTheDocument();
-    const input = getByDisplayValue(value);
-    expect(input).toBeInTheDocument();
-    const fieldSet = getByRole('radio', { name: 'Ja' })
-    expect(fieldSet?.getAttribute('aria-required')).toEqual('true');
-    expect(getByText(error.message)).toBeInTheDocument();
-  });
+    })
+    expect(getByText(helpText)).toBeInTheDocument()
+    expect(getByLabelText(options[0].text)).toBeInTheDocument()
+    expect(getByLabelText(options[1].text)).toBeInTheDocument()
+    const input = getByDisplayValue(value)
+    expect(input).toBeInTheDocument()
+    const fieldSet = getByRole('radio', {name: 'Ja'})
+    expect(fieldSet?.getAttribute('aria-required')).toEqual('true')
+    expect(getByText(error.message)).toBeInTheDocument()
+  })
+
+  test('Renders with button theme', async () => {
+    const {getByLabelText, getByDisplayValue, getByText, getByRole, queryByText} = render(
+      RadioGroup,
+      {
+        value,
+        name,
+        label: yesLabel,
+        helpText,
+        options,
+        isRequired: !!properties.validationRequired,
+        theme: 'button'
+      }
+    )
+    expect(getByText(helpText)).toBeInTheDocument()
+    expect(getByLabelText(options[0].text)).toBeInTheDocument()
+    expect(getByLabelText(options[1].text)).toBeInTheDocument()
+    const input = getByDisplayValue(value)
+    expect(input).toBeInTheDocument()
+    const fieldSet = getByRole('radio', {name: 'Ja'})
+    expect(fieldSet?.getAttribute('aria-required')).toEqual('true')
+    expect(queryByText(error.message)).not.toBeInTheDocument()
+    const label2 = getByLabelText(options[1].text)
+    await fireEvent.click(label2)
+    const label2AfterClick = getByLabelText(options[1].text)
+    expect(label2AfterClick.checked).toEqual(true)
+
+    const label1 = getByLabelText(options[0].text)
+    await fireEvent.click(label1)
+    const label1AfterClick = getByLabelText(options[0].text)
+    expect(label1AfterClick.checked).toEqual(true)
+    expect(label2AfterClick.checked).toEqual(false)
+  })
 
   test('Renders optional in label if not required', () => {
-    const err = {fieldName: name, message: 'This is the errormessage'};
+    const err = {fieldName: name, message: 'This is the errormessage'}
     const {getByText} = render(RadioGroup, {
       value,
       error: err,
@@ -51,7 +84,7 @@ describe('RadioGroup', () => {
       label: yesLabel,
       helpText,
       options
-    });
-    expect(getByText(/valgfritt felt/)).toBeInTheDocument();
-  });
-});
+    })
+    expect(getByText(/valgfritt felt/)).toBeInTheDocument()
+  })
+})
--- stories/components/form/RadioButtons.stories.svelte
+++ stories/components/form/RadioButtons.stories.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import {Meta, Story} from '@storybook/addon-svelte-csf';
-  import RadioGroup from '../../../src/svelte/form/RadioGroup.svelte';
-  import {wrapInShadowDom} from '../../utils';
+  import {Meta, Story} from '@storybook/addon-svelte-csf'
+  import RadioGroup from '../../../src/svelte/form/RadioGroup.svelte'
+  import {wrapInShadowDom} from '../../utils'
 
-  const name = 'radiobuttons';
+  const name = 'radiobuttons'
   const options = [
     {
       text: 'Dere kan kontakte meg',
@@ -13,7 +13,18 @@
       text: 'Jeg ønsker å være anonym',
       value: 'no'
     }
-  ];
+  ]
+  let buttonRadioValue
+  const buttonOptions = [
+    {
+      text: 'Ja',
+      value: '1'
+    },
+    {
+      text: 'Nei',
+      value: '2'
+    }
+  ]
 </script>
 
 <Meta
@@ -23,6 +34,12 @@
     helpText:
       'Beskriv kort og konkret hva du har observert og hvor alvorlig hendelsen er. Vær oppmerksom på den ansvarlige ofte får se meldingen.',
     errorMessage: 'Fyll inn dette feltet.',
+    buttonRadio: {
+      label: 'Reiser du selv med dyret?',
+      helpText: 'Hjelpetekst',
+      errorMessage: 'Fyll inn dette feltet.',
+      name: 'travelWithAnimal'
+    },
     disableCss: false
   }}
   argTypes={{
@@ -30,19 +47,40 @@
     helpText: {control: 'text'},
     errorMessage: {control: 'text'},
     disableCss: {control: 'boolean'}
-  }}
-/>
+  }} />
 
-<Story name="Normal" let:label let:helpText let:disableCss>
+<Story name="Normal" let:label let:helpText let:disableCss let:args>
   <div use:wrapInShadowDom={disableCss}>
+    <h1>Radioknapper</h1>
+    <h2>Theme - radio</h2>
     <form class="form-layout">
-      <RadioGroup {options} {name} {helpText} {label} error={undefined} textOptional="valgfritt" />
+      <RadioGroup
+        {options}
+        {name}
+        {helpText}
+        {label}
+        error={undefined}
+        textOptional="valgfritt"
+        theme="radio" />
+    </form>
+    <h2 id="theme">Theme - button</h2>
+    <form action="" class="form-layout">
+      <RadioGroup
+        options={buttonOptions}
+        bind:value={buttonRadioValue}
+        name={args.buttonRadio.name}
+        {helpText}
+        label={args.buttonRadio.label}
+        error={undefined}
+        textOptional={args.buttonRadio.textOptional}
+        theme="button" />
     </form>
   </div>
 </Story>
 
-<Story name="Radio with error" let:label let:helpText let:disableCss let:errorMessage>
+<Story name="Radio with error" let:label let:helpText let:disableCss let:errorMessage let:args>
   <div use:wrapInShadowDom={disableCss}>
+    <h2>Theme - radio</h2>
     <form class="form-layout">
       <RadioGroup
         {options}
@@ -50,8 +88,26 @@
         {helpText}
         {label}
         error={{key: name, message: errorMessage}}
-        textOptional="valgfritt"
-      />
+        textOptional="valgfritt" />
+    </form>
+    <h2 id="theme">Theme - button</h2>
+    <form class="form-layout">
+      <RadioGroup
+        options={buttonOptions}
+        bind:value={buttonRadioValue}
+        name={args.buttonRadio.name}
+        {helpText}
+        label={args.buttonRadio.label}
+        error={{key: name, message: errorMessage}}
+        textOptional={args.buttonRadio.textOptional}
+        theme="button" />
     </form>
   </div>
 </Story>
+
+<style lang="scss">
+  #theme {
+    margin-top: var(--spacer-medium);
+    margin-bottom: var(--spacer-xx-small);
+  }
+</style>
