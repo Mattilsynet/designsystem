4c5e7149ec7e662584172df966a2e886a31d0efd
Lisa <halvorsen.lisa@gmail.com>
2022-10-05 11:12:56 +0200

    Fix: support for printing

--- src/svelte/SummaryDetail.svelte
+++ src/svelte/SummaryDetail.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
   export let title: string
   export let detailsClass = ''
+  export let testId = ''
+  let isOpen = false
+
+  function beforePrint() {
+    isOpen = true
+  }
+  function afterPrint() {
+    isOpen = false
+  }
 </script>
 
-<details class={detailsClass}>
+<svelte:window on:beforeprint={beforePrint} on:afterPrint={afterPrint} />
+
+<details class={detailsClass} data-test-id={testId} bind:open={isOpen}>
   <summary>{@html title}</summary>
   <div class="summary-wrapper">
     <slot />
