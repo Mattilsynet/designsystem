9da63c46c86337387788b8e0d4cc3a73420eb4bf
Lisa Halvorsen <lisa.halvorsen@mattilsynet.no>
2022-10-19 12:07:54 +0200

    Fix: MT-1472 intro on guide (#580)
    * Fix: intro on guide
    
    - update chapter menu and chapter navigation with start index to start numbering on 0 or 1
    
    * Fix: adjust for design changes
    
    - move close button
    
    * Fix: Show chapter nav if there is more than 1 chapter

--- src/svelte/ChapterNavigation.svelte
+++ src/svelte/ChapterNavigation.svelte
@@ -6,15 +6,16 @@
   export let nextText = ''
   export let previousText = ''
   export let chapters: Array<Chapter> = []
-  export let currentChapterIndex = 0
+  export let currentChapterIndex: 0 | 1 = 0
+  export let startIndex = 0
   let className = ''
   export {className as class}
 
   $: nextChapterIndex = currentChapterIndex + 1
-  $: nextChapterNumber = nextChapterIndex + 1
+  $: nextChapterNumber = nextChapterIndex + startIndex
   $: nextChapter = chapters[nextChapterIndex]
   $: previousChapterIndex = currentChapterIndex - 1
-  $: previousChapterNumber = previousChapterIndex + 1
+  $: previousChapterNumber = previousChapterIndex + startIndex
   $: previousChapter = chapters[previousChapterIndex]
 
   function hasNextChapter(currentChapterNumber: number): boolean {
@@ -28,27 +29,29 @@
   const dispatch = createEventDispatcher<{chapterChange: ChapterChangeDetails}>()
 </script>
 
-<nav class="chapter-navigation {className}">
-  <a
-    href={nextChapter ? nextChapter.url : undefined}
-    on:click|preventDefault={dispatch('chapterChange', {index: nextChapterIndex})}
-    class="multi-line text-align-right {!hasNextChapter(currentChapterIndex)
-      ? 'inclusively-hidden-initial'
-      : ''}"
-    aria-disabled={!hasNextChapter(currentChapterIndex)}>
-    <span class="next-link">{nextText}</span>
-    {showChapterNumber ? `${nextChapterNumber}.` : ''}
-    {nextChapter ? nextChapter.heading : ''}
-  </a>
-  <a
-    href={previousChapter ? previousChapter.url : undefined}
-    class="multi-line {!hasPreviousChapter(currentChapterIndex)
-      ? 'inclusively-hidden-initial'
-      : ''}"
-    on:click|preventDefault={dispatch('chapterChange', {index: previousChapterIndex})}
-    aria-disabled={!hasPreviousChapter(currentChapterIndex)}>
-    <span class="previous-link">{previousText}</span>
-    {showChapterNumber ? `${previousChapterNumber}.` : ''}
-    {previousChapter ? previousChapter.heading : ''}
-  </a>
-</nav>
+{#if chapters.length > 1}
+  <nav class="chapter-navigation {className}">
+    <a
+      href={nextChapter ? nextChapter.url : undefined}
+      on:click|preventDefault={dispatch('chapterChange', {index: nextChapterIndex})}
+      class="multi-line text-align-right {!hasNextChapter(currentChapterIndex)
+        ? 'inclusively-hidden-initial'
+        : ''}"
+      aria-disabled={!hasNextChapter(currentChapterIndex)}>
+      <span class="next-link">{nextText}</span>
+      {showChapterNumber ? `${nextChapterNumber}.` : ''}
+      {nextChapter ? nextChapter.heading : ''}
+    </a>
+    <a
+      href={previousChapter ? previousChapter.url : undefined}
+      class="multi-line {!hasPreviousChapter(currentChapterIndex)
+        ? 'inclusively-hidden-initial'
+        : ''}"
+      on:click|preventDefault={dispatch('chapterChange', {index: previousChapterIndex})}
+      aria-disabled={!hasPreviousChapter(currentChapterIndex)}>
+      <span class="previous-link">{previousText}</span>
+      {showChapterNumber ? `${previousChapterNumber}.` : ''}
+      {previousChapter ? previousChapter.heading : ''}
+    </a>
+  </nav>
+{/if}
---
+++ src/svelte/ChapterNavigation.test.ts
@@ -0,0 +1,81 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {render} from '@testing-library/svelte'
+import ChapterNavigation from './ChapterNavigation.svelte'
+
+describe('Chapter Navigation', () => {
+  const chapters = [
+    {url: '1', heading: 'Intro', index: 0},
+    {url: '2', heading: 'Chapter 1', index: 1},
+    {url: '3', heading: 'Chapter 2', index: 2}
+  ]
+
+  const componentOptions = {
+    showChapterNumbers: false,
+    nextText: '',
+    previousText: '',
+    chapters: chapters,
+    currentChapterIndex: 0,
+    startIndex: 0
+  }
+  test('Renders with defaults - first page', () => {
+    const {getByText, queryByText} = render(ChapterNavigation, componentOptions)
+    expect(queryByText('Intro')).not.toBeInTheDocument()
+    expect(getByText('Chapter 1')).toBeInTheDocument()
+    expect(queryByText('Chapter 2')).not.toBeInTheDocument()
+  })
+
+  test('Renders from middle', () => {
+    const {getByText, queryByText} = render(ChapterNavigation, {
+      ...componentOptions,
+      currentChapterIndex: 1
+    })
+    expect(getByText('Intro')).toBeInTheDocument()
+    expect(queryByText('Chapter 1')).not.toBeInTheDocument()
+    expect(getByText('Chapter 2')).toBeInTheDocument()
+  })
+
+  test('Renders from last page', () => {
+    const {getByText, queryByText} = render(ChapterNavigation, {
+      ...componentOptions,
+      currentChapterIndex: 2
+    })
+    expect(queryByText('Intro')).not.toBeInTheDocument()
+    expect(getByText('Chapter 1')).toBeInTheDocument()
+    expect(queryByText('Chapter 2')).not.toBeInTheDocument()
+  })
+
+  test('Renders with button text', () => {
+    const {getByText} = render(ChapterNavigation, {
+      ...componentOptions,
+      nextText: 'Next',
+      previousText: 'Previous',
+      currentChapterIndex: 1
+    })
+    expect(getByText(/Next/)).toBeInTheDocument()
+    expect(getByText(/Previous/)).toBeInTheDocument()
+  })
+
+  test('Renders with numbers', () => {
+    const {getByText, queryByText} = render(ChapterNavigation, {
+      ...componentOptions,
+      showChapterNumber: true,
+      currentChapterIndex: 1
+    })
+    expect(getByText('0. Intro')).toBeInTheDocument()
+    expect(queryByText('1. Chapter 1')).not.toBeInTheDocument()
+    expect(getByText('2. Chapter 2')).toBeInTheDocument()
+  })
+
+  test('Dont show chapter nav if there is 1 chapter', () => {
+    const {queryByText} = render(ChapterNavigation, {
+      ...componentOptions,
+      chapters: componentOptions.chapters.splice(0, 1)
+    })
+    expect(queryByText('0. Intro')).not.toBeInTheDocument()
+    expect(queryByText('1. Chapter 1')).not.toBeInTheDocument()
+    expect(queryByText('2. Chapter 2')).not.toBeInTheDocument()
+  })
+})
