28257108ef4bcfd617c3f41a4bcbb2fac4709797
Amund Lindstøl <alindstol@gmail.com>
2022-11-08 11:59:35 +0100

    MTP-1078: added alternative display type of highlighted content

--- src/svelte/HighlightedContentLink.svelte
+++ src/svelte/HighlightedContentLink.svelte
@@ -7,26 +7,44 @@
   export {className as class}
   export let image: {src: string; alt: string | undefined} | undefined = undefined
   export let lang = 'NO-nb'
-  export let heading: string | undefined
+  export let title: string | undefined
+  export let shortTitle: string | undefined
+  export let displayType: string | undefined
   export let isExternal = false
   export let headingClass = ''
   export let headerTag: 'h2' | 'h3' = 'h2'
 </script>
 
-<a
-  href={url}
-  class="hightlighted-content {className}"
-  rel={isExternal ? 'external' : undefined}
-  data-testid="hightlighted-content-link">
-  {#if image && image.src}
-    <img src={image.src} alt={image.alt} />
-  {/if}
-  <span class="layout-flex layout-flex-col highlighted-text" style="gap:0">
-    {#if heading}
-      <HeadingLevel class="heading {headingClass}" headingLevel={+headerTag.charAt(1)}>
-        {heading}
-      </HeadingLevel>
-    {/if}
+{#if displayType === 'blue'}
+  <div class="col-1-span-12 layout-flex layout-flex-col border-radius p-xs bg-lighter-light-blue">
+    <HeadingLevel class="heading {headingClass}" headingLevel={+headerTag.charAt(1)}>
+      {title}
+    </HeadingLevel>
     <slot />
-  </span>
-</a>
+    <a
+      href={url}
+      class="button button--primary button--space-between forward-arrow-end padded"
+      rel={isExternal ? 'external' : undefined}
+      data-testid="hightlighted-content-link">
+      {shortTitle}
+    </a>
+  </div>
+{:else}
+  <a
+    href={url}
+    class="hightlighted-content {className}"
+    rel={isExternal ? 'external' : undefined}
+    data-testid="hightlighted-content-link">
+    {#if image && image.src}
+      <img src={image.src} alt={image.alt} />
+    {/if}
+    <span class="layout-flex layout-flex-col highlighted-text" style="gap:0">
+      {#if shortTitle}
+        <HeadingLevel class="heading {headingClass}" headingLevel={+headerTag.charAt(1)}>
+          {shortTitle}
+        </HeadingLevel>
+      {/if}
+      <slot />
+    </span>
+  </a>
+{/if}
