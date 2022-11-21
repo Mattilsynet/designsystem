3df283c0725574cfb905df102dd50c876bbde75a
Amund Lindstøl <alindstol@gmail.com>
2022-11-21 08:35:05 +0100

    MTP-1078_highlighted_content_background (#625)
    * MTP-1078: added alternative display type of highlighted content
    
    * MTP-1078: added literal union & fixed tests
    
    * MTP-1078: Added story for blue type
    
    * MTP-1078: Fixed rendering error with babel-loader downgrade

--- src/svelte/HighlightedContentLink.svelte
+++ src/svelte/HighlightedContentLink.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import HeadingLevel from './HeadingLevel.svelte'
+  import {mapRelExternal} from '../ts/utils'
 
   let url = ''
   let className = ''
@@ -7,26 +8,47 @@
   export {className as class}
   export let image: {src: string; alt: string | undefined} | undefined = undefined
   export let lang = 'NO-nb'
-  export let heading: string | undefined
-  export let isExternal = false
+  export let title: string | undefined
+  export let shortTitle: string | undefined
+  export let displayType: HighlightedContentDisplayType | undefined
   export let headingClass = ''
   export let headerTag: 'h2' | 'h3' = 'h2'
+
+  type HighlightedContentDisplayType = 'white' | 'blue'
 </script>
 
-<a
-  href={url}
-  class="hightlighted-content {className}"
-  rel={isExternal ? 'external' : undefined}
-  data-testid="hightlighted-content-link">
-  {#if image && image.src}
-    <img src={image.src} alt={image.alt} />
-  {/if}
-  <span class="layout-flex layout-flex-col highlighted-text" style="gap:0">
-    {#if heading}
-      <HeadingLevel class="heading {headingClass}" headingLevel={+headerTag.charAt(1)}>
-        {heading}
-      </HeadingLevel>
-    {/if}
+{#if displayType === 'blue'}
+  <div
+    class="col-1-span-12 layout-flex layout-flex-col border-radius p-xs bg-lighter-light-blue"
+    data-testid="highlighted-content-link-blue">
+    <HeadingLevel class="heading {headingClass}" headingLevel={+headerTag.charAt(1)}>
+      {title}
+    </HeadingLevel>
     <slot />
-  </span>
-</a>
+    <a
+      href={url}
+      rel={mapRelExternal(url)}
+      class="button button--primary button--space-between forward-arrow-end padded"
+      data-testid="highlighted-content-link">
+      {shortTitle}
+    </a>
+  </div>
+{:else}
+  <a
+    href={url}
+    rel={mapRelExternal(url)}
+    class="highlighted-content {className}"
+    data-testid="highlighted-content-link">
+    {#if image && image.src}
+      <img src={image.src} alt={image.alt} />
+    {/if}
+    <span class="layout-flex layout-flex-col highlighted-text" style="gap:0">
+      {#if shortTitle}
+        <HeadingLevel class="heading {headingClass}" headingLevel={+headerTag.charAt(1)}>
+          {shortTitle}
+        </HeadingLevel>
+      {/if}
+      <slot />
+    </span>
+  </a>
+{/if}
--- src/svelte/HighlightedContentLink.test.ts
+++ src/svelte/HighlightedContentLink.test.ts
@@ -6,30 +6,55 @@ import {render} from '@testing-library/svelte'
 import HighlightedContent from './HighlightedContentLink.svelte'
 
 describe('Highlighted content', () => {
-  test('Renders', () => {
+  test('Renders default display type', () => {
     const componentOptions = {
-      heading: 'This is the heading',
+      title: 'This is the title',
+      shortTitle: 'This is the short title',
+      href: 'http',
+      image: {src: 'test', alt: 'alt text'}
+    }
+    const {getByTestId, getByText, getByAltText, queryByText} = render(
+      HighlightedContent,
+      componentOptions
+    )
+    expect(queryByText('This is the title')).not.toBeInTheDocument()
+    expect(getByText('This is the short title')).toBeInTheDocument()
+    expect(getByAltText('alt text')).toBeInTheDocument()
+    const link = getByTestId('highlighted-content-link')
+    expect(link.getAttribute('rel')).toEqual('external')
+    const headerElement = getByText('This is the short title')
+    expect(headerElement.tagName).toEqual('H2')
+  })
+
+  test('Renders blue display type', () => {
+    const componentOptions = {
+      title: 'This is the title',
+      shortTitle: 'This is the short title',
+      displayType: 'blue',
       isExternal: true,
       image: {src: 'test', alt: 'alt text'}
     }
-    const {getByTestId, getByText, getByAltText} = render(HighlightedContent, componentOptions)
-    expect(getByText('This is the heading')).toBeInTheDocument()
-    expect(getByAltText('alt text')).toBeInTheDocument()
-    const link = getByTestId('hightlighted-content-link')
-    expect(link.getAttribute('rel')).toEqual('external')
-    const headerElement = getByText('This is the heading')
+    const {getByText, queryByAltText} = render(HighlightedContent, componentOptions)
+    expect(getByText('This is the title')).toBeInTheDocument()
+    expect(getByText('This is the short title')).toBeInTheDocument()
+    expect(queryByAltText('alt text')).not.toBeInTheDocument()
+    const headerElement = getByText('This is the title')
     expect(headerElement.tagName).toEqual('H2')
+    const headerElement2 = getByText('This is the short title')
+    expect(headerElement2.tagName).toEqual('A')
   })
+
   test('Renders without props', () => {
     const {queryByText, queryByAltText, getByTestId} = render(HighlightedContent, {})
     expect(queryByText('This is the heading')).not.toBeInTheDocument()
     expect(queryByAltText('alt text')).not.toBeInTheDocument()
-    const link = getByTestId('hightlighted-content-link')
+    const link = getByTestId('highlighted-content-link')
     expect(link.getAttribute('rel')).toEqual(null)
   })
+
   test('Render with h3 title', () => {
     const componentOptions = {
-      heading: 'This is the heading',
+      shortTitle: 'This is the heading',
       isExternal: true,
       image: {src: 'test', alt: 'alt text'},
       headerTag: 'h3'
