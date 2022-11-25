08159abdc36bc82d273099db7cccce0db729dc21
Amund Lindstøl <alindstol@gmail.com>
2022-11-25 13:17:26 +0100

    MTP-483_menupoints_and_keywords (#646)
    * MTP-483: added menupoints
    
    * MTP-483: Added iconResource
    
    * MTP-483: added styling and changed component structure
    
    * MTP-483: cleaned up css
    
    * MTP-483: supports intro as keyword
    
    * MTP-483: Added test for menu points

---
+++ src/svelte/MenuPoints.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import {mapRelExternal} from '../ts/utils'
+  import HeadingLevel from './HeadingLevel.svelte'
+
+  export let title = ''
+  export let headerTag: 'h2' | 'h3' | 'h4' | 'h5' | 'h6' = 'h3'
+  export let menuPoints = []
+</script>
+
+<div class="menu-points" data-testid="menu-points">
+  <h2 class="m-t-xxs">{@html title}</h2>
+  <div
+    class="layout-grid layout-grid--column-3 row-gap"
+    style="--space-section:var(--spacer-medium)">
+    {#each menuPoints as menuPoint, index}
+      <div class="menu-point layout-flex-col layout-flex-col--x-small">
+        <div class="menu-point--title">
+          <div class="menu-point--icon">
+            {#if menuPoint.iconResource}
+              {@html menuPoint.iconResource}
+            {:else if menuPoint.icon}
+              <img src={menuPoint.icon} alt="" data-testid="img-icon"/>
+            {/if}
+          </div>
+          <a
+            href={menuPoint.url}
+            rel={mapRelExternal(menuPoint.url)}
+            class="animated-header-arrow-after no-underline flex"
+            data-testid="menupoints-link">
+            <HeadingLevel class={headerTag} headingLevel={+headerTag.charAt(1)}>
+              {menuPoint.displayName}
+            </HeadingLevel>
+          </a>
+        </div>
+        <p>
+          {@html menuPoint.keywords?.length > 0
+            ? menuPoint.keywords
+            : '[Legg til stikkord på innholdet]'}
+        </p>
+      </div>
+    {/each}
+  </div>
+</div>
