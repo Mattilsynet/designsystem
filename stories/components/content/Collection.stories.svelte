566b95767fc0cc165da2060ac5cb41332c64c48d
Lisa <halvorsen.lisa@gmail.com>
2022-11-30 09:44:54 +0100

    fix: MTP-1189 table in collection
    - update story

--- stories/components/content/Collection.stories.svelte
+++ stories/components/content/Collection.stories.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <Meta
-  title="Innhold/Regelveiledning samleside"
+  title="Innhold/Samleside"
   args={{
     title: 'Regelveiledning samleside overskrift',
     intro: 'Regelveiledning samleside ingress',
@@ -33,7 +33,22 @@
               En voksen person må være hovedansvarlig for dyret. I følge dyrevelferdsloven kan barn under 16 år ikke ha et
               selvstendig ansvar for dyr.
             </li>
-          </ul>`,
+          </ul>
+          <table class="responsive-table col-2-span-10" role="table">
+          <caption>Merkeregler for flaskevann</caption>
+          <thead role="rowgroup">
+          <tr role="row"><th role="columnheader" scope="col">Produkt/merking med</th>
+          <th role="columnheader" scope="col">Handelsnavn/varenavn/varemerke</th>
+          <th role="columnheader" scope="col">Kilde/utvinningssted for kilden (for naturlig mineralvann/kildevann)</th>
+          <th role="columnheader" scope="col">Varebetegnelse</th></tr></thead>
+          <tbody><tr role="row">
+          <td class="text" role="cell"><span class="responsive-table__heading" aria-hidden="true">Produkt/merking med</span>Naturlig mineralvann/kildevann</td>
+          <td class="text" role="cell"><span class="responsive-table__heading" aria-hidden="true">Handelsnavn/varenavn/varemerke</span>Skal ikke frambys under mer enn ett handelsnavn/varenavn.</td>
+          <td class="text" role="cell"><span class="responsive-table__heading" aria-hidden="true">Kilde/utvinningssted for kilden (for naturlig mineralvann/kildevann)</span>Navnet på kilden/utvinningsstedet skal være dominerende.</td>
+          <td class="text" role="cell"><span class="responsive-table__heading" aria-hidden="true">Varebetegnelse</span>Naturlig mineralvann eller kildevann.<br><br>Varebetegnelsen naturlig mineralvann tillates bare på vann som har et naturlig mineralinnhold som avviker fra kildevann og drikkevann.</td></tr>
+          </tbody></table>
+
+          `,
         howToDoItTitle: 'Slik gjør du',
         howToDoIt: `<p>Slik gjør du beskrivelse</p>`
       },
@@ -87,7 +102,7 @@
       {#each legalItems as legal}
         <article
           id={toKebabCase(legal.title)}
-          class="legal-collection legal-collection__border-top col-3-span-6"
+          class="legal-collection legal-collection__border-top col-1-span-12"
           aria-labelledby="collection-title-1">
           <h2 id="collection-title-1">{legal.title}</h2>
 
@@ -97,9 +112,7 @@
 
           {@html legal.text}
 
-          <section
-            class="layout-flex-col layout-flex-col--x-small"
-            aria-labelledby="how-to-heading-1">
+          <section class="article-page children-full-width" aria-labelledby="how-to-heading-1">
             <h3 id="how-to-heading-1">
               {legal.howToDoItTitle}
             </h3>
