f49a5412c38fd7c2437d8734dfabc9620268c5c5
Amund Lindstøl <alindstol@gmail.com>
2022-12-01 10:30:39 +0100

    Added story for banner image

---
+++ stories/components/BannerImage.stories.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import {Meta, Story} from '@storybook/addon-svelte-csf'
+</script>
+
+<Meta
+  title="Components/Banner Image"
+  args={{
+    bannerImage: 'testbilde.jpg',
+    disabled: false,
+    disableJs: false,
+    disableCss: false
+  }}
+  argTypes={{
+    disabled: {control: 'boolean'},
+    disableJs: {control: 'boolean'},
+    disableCss: {control: 'boolean'}
+  }} />
+
+<Story name="Normal" let:bannerImage let:disableCss let:disabled let:secondary>
+  <h1>Toppbilde</h1>
+  <div class="banner-image" style={`--banner-image-url: url(${bannerImage})`}>
+    <div class="layout-grid layout-grid--column-3 container banner-image--layout">
+      <div class="bg-light-light-blue p-xxs">
+        Innhold innenfor bilde. Skjuler bilde for mobilvisning.
+      </div>
+    </div>
+  </div>
+</Story>
