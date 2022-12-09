bdcc007278c666f3af111b61abdcc63caefa39fb
Amund Lindstøl <alindstol@gmail.com>
2022-12-09 16:30:07 +0100

    MTP-431: created another CardArticle type & added stories

--- src/svelte/components/CardArticle.svelte
+++ src/svelte/components/CardArticle.svelte
@@ -8,14 +8,18 @@
   export let text: string | undefined
   export let linkUrl: string | undefined
   export let linkText: string | undefined
-  export let type: 'task' | 'form-intro-module' | 'legal-text' | '' = ''
+  export let type: 'task' | 'form-intro-module' | 'legal-text' | 'highlighted-link' | '' = ''
   export let headerTag: 'h2' | 'h3' | 'h4' | 'h5' | 'h6' = 'h3' // Fallback is h3
+  export let iconClass: 'info-icon' | 'warning-icon' | 'task-icon' | '' =
+    type === 'task' ? 'task-icon' : ''
+
+  const isButton = type !== 'highlighted-link'
 </script>
 
 <article class={type} aria-labelledby={headingId}>
   <HeadingLevel
     id={headingId}
-    class="title {headerTag === 'h2' ? 'h3' : ''}"
+    class="title {iconClass} {headerTag === 'h2' ? 'h3' : ''}"
     headingLevel={+headerTag.charAt(1)}>
     {@html title}
   </HeadingLevel>
@@ -33,8 +37,14 @@
   {/if}
 
   {#if linkUrl && linkText}
-    <a href={linkUrl} rel={mapRelExternal(linkUrl)} class="button button--primary self-start">
-      {linkText}
+    <a
+      href={linkUrl}
+      rel={mapRelExternal(linkUrl)}
+      class:button={isButton}
+      class:button--primary={isButton}
+      class:self-start={isButton}
+      class:forward-arrow-after={!isButton}>
+      {@html linkText}
     </a>
   {/if}
 </article>
---
+++ stories/content/HighlightedLink.stories.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  import {Meta, Story} from '@storybook/addon-svelte-csf'
+  import {wrapInShadowDom} from '../utils'
+  import CardArticle from '../../src/svelte/components/CardArticle.svelte'
+</script>
+
+<Meta
+  title="Innhold/Fremhevet lenke"
+  args={{
+    tasks: [
+      {
+        title: 'Tittel med ingen ikon',
+        text: `<p>Norge får nytt regelverk om dyrehelse i form av tolv nye forskrifter.
+        Forskriftene ble fastsatt 6. april og vil tre i kraft i slutten av april.
+        Samtidig vil en rekke gjeldende forskrifter på dyrehelseområdet oppheves.</p>`,
+        nextActionUrl: 'http://',
+        nextActionLinkText: 'Mer informasjon om nytt regelverk',
+        headerTag: 'h3',
+        iconClass: ''
+      },
+      {
+        title: 'Tittel med info ikon',
+        text: `<p>Noe tekst</p>`,
+        nextActionUrl: 'http://',
+        nextActionLinkText: 'En lenke',
+        iconClass: 'info-icon'
+      },
+      {
+        title: 'Tittel med advarsel ikon',
+        text: `<p>Noe tekst</p>`,
+        nextActionUrl: 'http://',
+        nextActionLinkText: 'En lenke',
+        iconClass: 'warning-icon'
+      },
+      {
+        title: 'Manglende lenke',
+        text: `<p>Noe tekst</p>`,
+        nextActionLinkText: 'En lenke'
+      }
+    ],
+    disableCss: false
+  }}
+  argTypes={{
+    tasks: {control: 'array'},
+    disableCss: {control: 'boolean'}
+  }} />
+
+<Story name="Normal" let:tasks let:disableCss>
+  <div use:wrapInShadowDom={disableCss} class="container layout-flex-col layout-flex-col--x-small">
+    {#each tasks as task}
+      <CardArticle
+        type="highlighted-link"
+        linkText={task.nextActionLinkText}
+        linkUrl={task.nextActionUrl}
+        title={task.title}
+        text={task.text}
+        id={task.title}
+        headerTag={task.headerTag}
+        iconClass={task.iconClass} />
+    {/each}
+  </div>
+</Story>
+
+<style lang="scss">
+  main {
+    --gap: var(--spacer-x-small);
+  }
+  h2 {
+    margin-top: var(--spacer-small);
+  }
+  section {
+    --gap: var(--spacer-xx-small);
+  }
+</style>
