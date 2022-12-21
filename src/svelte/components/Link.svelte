7e8727be7dd763553d14f376d95dec2c1a1afe12
Lisa <halvorsen.lisa@gmail.com>
2022-12-21 11:19:58 +0100

    MTP-1214: remove extra style

--- src/svelte/components/Link.svelte
+++ src/svelte/components/Link.svelte
@@ -7,7 +7,6 @@
   export {className as class}
   export let style = ''
   export let fileName = ''
-  export let style = ''
 
   $: fileType = getFileExtension(fileName) ? `(${getFileExtension(fileName)})` : ''
 </script>
