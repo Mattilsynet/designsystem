4d83d681c2ec24058f886efdd89075429b4b9a0b
Lisa <halvorsen.lisa@gmail.com>
2023-02-21 08:31:15 +0100

    Set up for design tokens

---
+++ tokens/config/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  // source: ["src/index.js", "src/**/*.json"],
+  source: ["./src/*.json"],
+  platforms: {
+    scss: {
+      transformGroup: "scss",
+      buildPath: "dist/",
+      files: [
+        {
+          destination: "tokens.scss",
+          format: "scss/variables",
+        },
+      ],
+    },
+    css: {
+      transformGroup: "css",
+      buildPath: "dist/",
+      files: [
+        {
+          destination: "tokens.css",
+          format: "css/variables",
+          options: {
+            outputReferences: true,
+            selector: ":root, :host",
+          },
+        },
+      ],
+    },
+  },
+};
---
+++ tokens/src/index.js
@@ -0,0 +1,49 @@
+const baseFontSize = 16;
+const getFontSize = (size) => `${size / baseFontSize}rem`;
+
+module.exports = {
+  a: {
+    font: {
+      family: { value: '"Source Sans Pro", Arial, sans-serif' },
+      line: {
+        height: {
+          heading: {
+            "2xlarge": { value: getFontSize(52) },
+            xlarge: { value: getFontSize(40) },
+            large: { value: getFontSize(36) },
+            medium: { value: getFontSize(32) },
+            small: { value: getFontSize(28) },
+            xsmall: { value: getFontSize(24) },
+          },
+          xlarge: { value: getFontSize(28) },
+          large: { value: getFontSize(24) },
+          medium: { value: getFontSize(20) },
+        },
+      },
+      size: {
+        heading: {
+          "2xlarge": { value: getFontSize(40) },
+          xlarge: { value: getFontSize(32) },
+          large: { value: getFontSize(28) },
+          medium: { value: getFontSize(24) },
+          small: { value: getFontSize(20) },
+          xsmall: { value: getFontSize(18) },
+        },
+        xlarge: { value: getFontSize(20) },
+        large: { value: getFontSize(18) },
+        medium: { value: getFontSize(16) },
+        small: { value: getFontSize(14) },
+      },
+      weight: {
+        bold: { value: "600" },
+        regular: { value: "400" },
+      },
+    },
+    "z-index": {
+      modal: { value: 2000 },
+      popover: { value: 1000 },
+      focus: { value: 10 },
+      tooltip: { value: 3000 },
+    },
+  },
+};
