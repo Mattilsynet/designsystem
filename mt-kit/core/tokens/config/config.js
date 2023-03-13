0bbd2701da2721326eeabe2200305d97dad4dd20
Lisa <halvorsen.lisa@gmail.com>
2023-03-13 08:27:23 +0100

    add js to out put for colors

--- mt-kit/core/tokens/config/config.js
+++ mt-kit/core/tokens/config/config.js
@@ -1,30 +1,40 @@
 module.exports = {
   // source: ["src/index.js", "src/**/*.json"],
-  source: ["../src/output.json"],
+  source: ['../src/output.json'],
   platforms: {
     scss: {
-      transformGroup: "scss",
-      buildPath: "../build/",
+      transformGroup: 'scss',
+      buildPath: '../build/',
       files: [
         {
-          destination: "tokens.scss",
-          format: "scss/variables",
-        },
-      ],
+          destination: 'tokens.scss',
+          format: 'scss/variables'
+        }
+      ]
     },
     css: {
-      transformGroup: "css",
-      buildPath: "../build/",
+      transformGroup: 'css',
+      buildPath: '../build/',
       files: [
         {
-          destination: "tokens.css",
-          format: "css/variables",
+          destination: 'tokens.css',
+          format: 'css/variables',
           options: {
             outputReferences: true,
-            selector: ":root, :host",
-          },
-        },
-      ],
+            selector: ':root, :host'
+          }
+        }
+      ]
     },
-  },
-};
+    js: {
+      transformGroup: 'js',
+      buildPath: '../build/',
+      files: [
+        {
+          destination: 'tokens.js',
+          format: 'javascript/module-flat'
+        }
+      ]
+    }
+  }
+}
