b58b6870d351c4373bcb6ca70b3131ec1d1334aa
Alexander Enge <alexander.heireth.enge@mattilsynet.no>
2023-03-21 12:47:31 +0100

    Included text for screen readers when LoadingButton component is in loading state.

--- mt-kit/core/react/src/button/LoadingButton.tsx
+++ mt-kit/core/react/src/button/LoadingButton.tsx
@@ -6,16 +6,28 @@ interface LoadingButtonProps extends ButtonProps {
    * Show a loading spinner on the button
    */
   isLoading?: boolean
+  /**
+   * Hidden text used by screen reader when isLoading is true
+   */
+  hiddenLoadingText?: string
 }
 
 const LoadingButton: React.FC<LoadingButtonProps> = ({
   isLoading,
+  hiddenLoadingText = 'Loading',
   children,
   ...rest
 }): JSX.Element => (
   <Button {...rest}>
     <div style={{ visibility: isLoading === true ? 'hidden' : 'visible' }}>{children}</div>
-    {isLoading === true && <span style={{ position: 'absolute' }} className="spinner" />}
+    <div role="status" aria-live="polite" style={{ position: 'absolute' }}>
+      {isLoading === true && (
+        <>
+          <span className="spinner" />
+          <span className="inclusively-hidden">{hiddenLoadingText}</span>
+        </>
+      )}
+    </div>
   </Button>
 )
 
