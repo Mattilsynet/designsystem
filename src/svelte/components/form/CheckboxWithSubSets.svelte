6df17b89066486c37141489553edd752cb3889e9
Lisa Halvorsen <lisa.halvorsen@mattilsynet.no>
2023-05-19 11:30:11 +0200

    MTP-1534 fe huk av alle tema funksjonalitet (#863)
    * MTP-1534: add checkAll
    
    - refactor. rename fields. use only options. use input checked. remove unused functions
    
    * MTP-1534: add checked to type
    
    * MTP-1534: add util
    
    * MTP-1534: update story
    
    * MTP-1534: remove default in level2legend

--- src/svelte/components/form/CheckboxWithSubSets.svelte
+++ src/svelte/components/form/CheckboxWithSubSets.svelte
@@ -4,130 +4,85 @@
   import type {CheckboxWithSubSectionsOptions} from '../../../ts/types'
   import {interpolate} from '../../../ts/utils'
 
-  export let params: Record<string, string> = {}
-  export let name: string
   let className = ''
   export {className as class}
-  export let legend: string
+  export let level1Legend: string
   export let variation: 'primary' | 'secondary' = 'primary'
   export let options: Array<CheckboxWithSubSectionsOptions>
-  export let categoryName = 'kategori'
-  export let subCategoryName = 'underkategori'
-  export let subCategoryLegend = `${legend} i`
+  export let optionsName = 'kategori'
+  export let hasCheckAll = false
+  export let checkAllLabel = 'Velg alle'
+  export let checkAllValue: string | undefined = undefined
+  export let level2Legend = ``
   export let helpText: string | undefined
-  export const checkboxes = {
-    reset() {
-      selectedCategoryValues = []
-      selectedSubCategoryValues = []
-    }
-  }
 
   $: fieldsetClass =
     variation === 'primary' ? 'checkbox-subsets--primary' : 'checkbox-subsets--secondary'
-
-  interface CheckboxWithSubSectionsStates {
-    key: string
-    docCount: number
-    displayName: string
-    checked: boolean
-    children?: Array<CheckboxWithSubSectionsStates>
-  }
+  let inputCheckedAll = false
+  $: inputCheckedAll = false
 
   let hasJS = false
-  // selectedCategoryValues and selectedSubCategoryValues are the values from the checked checkboxes.
-  // When binding the variables to the checkboxes, the checkboxes will be checked if the value is in the array.
-  let selectedCategoryValues = params[categoryName] ? (params[categoryName] as Array<string>) : []
-  let selectedSubCategoryValues = params[subCategoryName]
-    ? (params[subCategoryName] as Array<string>)
-    : []
 
-  $: states = []
-  let checkboxDOMElements = []
-
-  $: if (options) {
-    states = mapOptionsToState(options)
-  }
   onMount(() => {
-    states = mapOptionsToState(options)
     hasJS = true
   })
 
-  /*
-   * */
-  function mapOptionsToState(
-    opts: Array<CheckboxWithSubSectionsOptions>
-  ): Array<CheckboxWithSubSectionsStates> {
-    return opts.map(option => {
-      return {
-        key: option.key,
-        docCount: option.docCount,
-        displayName: option.displayName,
-        checked: selectedCategoryValues
-          ? selectedCategoryValues.filter(val => val === option.key).length > 0
-          : false,
-        children: option.children?.map(child => {
-          const childKey = `${option.key}/${child.key}`
-          return {
-            key: childKey,
-            docCount: child.docCount,
-            displayName: child.displayName,
-            checked:
-              (selectedSubCategoryValues && selectedSubCategoryValues.indexOf(child.key) >= 0) ??
-              false
-          }
-        })
-      }
-    })
-  }
-
   function mainCategory(mainIndex: number): void {
-    states[mainIndex].checked = !states[mainIndex].checked
     // Uncheck all subcategories if parent main category is unchecked
-    if (!states[mainIndex].checked) {
-      // To uncheck a checkbox, remove the value from the selectedSubCategoryValues array
-      states[mainIndex].children.forEach(subCategory => {
+    if (!options[mainIndex].checked) {
+      options[mainIndex].children.forEach(subCategory => {
         subCategory.checked = false
-        selectedSubCategoryValues = selectedSubCategoryValues.filter(value => {
-          return value !== subCategory.key
-        })
       })
     }
+
+    inputCheckedAll = options.filter((s) => s.checked).length === options.length
   }
+
   function formatLabel(displayName: string, docCount?: number): string {
     return `${displayName} ${docCount ? `(${docCount})` : ''}`
   }
 
-  function subCategory(mainIndex: number, subCategoryKey: string): void {
-    // uncheck subcategory in the state list
-    states[mainIndex].children = states[mainIndex].children.map(child => {
-      if (child.key === subCategoryKey) {
-        return {
-          ...child,
-          checked: !child.checked
-        }
+  function checkAll(e: Event) {
+    options = options.map(state => {
+      return {
+        ...state,
+        checked: e.target.checked,
       }
-      return child
     })
   }
 </script>
 
 <fieldset class={className}>
   <legend id="legend" class="border" class:inclusively-hidden={variation === 'secondary'}>
-    {legend}
+    {level1Legend}
   </legend>
   {#if helpText}
     <p class="hint">{helpText}</p>
   {/if}
-  {#each states as listItem, mainIndex}
+  {#if hasJS && hasCheckAll}
+    <div class="form-control checkbox m-b-xxs">
+      <input
+        id={checkAllValue}
+        type="checkbox"
+        class="input__control"
+        name={optionsName}
+        value={checkAllValue}
+        bind:checked={inputCheckedAll}
+        on:change={e => checkAll(e)} />
+      <label for={checkAllValue}>
+        {checkAllLabel}
+      </label>
+    </div>
+  {/if}
+  {#each options as listItem, mainIndex}
     <div class="form-control checkbox narrow" class:m-t-xxs={mainIndex > 0}>
       <input
         id={listItem.key}
         type="checkbox"
         class="input__control"
-        name={categoryName}
-        bind:this={checkboxDOMElements[mainIndex]}
-        bind:group={selectedCategoryValues}
+        name={optionsName}
         value={listItem.key}
+        bind:checked={listItem.checked}
         aria-checked={listItem.checked}
         on:change={() => mainCategory(mainIndex)} />
       <label for={listItem.key}>
@@ -137,7 +92,7 @@
     {#if (!hasJS || listItem.checked) && listItem.children && listItem.children.length > 0}
       <fieldset class={fieldsetClass} transition:slide|local={{y: 200, duration: 200}}>
         <legend>
-          {interpolate(subCategoryLegend, [listItem.displayName.toLowerCase()])}
+          {interpolate(level2Legend, [listItem.displayName.toLowerCase()])}
         </legend>
         {#each listItem.children as subListItem, subListIndex}
           <div
@@ -147,12 +102,12 @@
             <input
               id={subListItem.key}
               type="checkbox"
-              name={subCategoryName}
+              name={optionsName}
               class="input__control"
               value={subListItem.key}
+              bind:checked={subListItem.checked}
               aria-checked={subListItem.checked}
-              bind:group={selectedSubCategoryValues}
-              on:change={() => subCategory(mainIndex, subListItem.key)} />
+              />
             <label for={subListItem.key}>
               {formatLabel(subListItem.displayName, subListItem.docCount)}
             </label>
--- src/svelte/components/form/CheckboxWithSubSets.test.ts
+++ src/svelte/components/form/CheckboxWithSubSets.test.ts
@@ -3,9 +3,10 @@
  */
 import {fireEvent, render, waitFor} from '@testing-library/svelte'
 import CheckboxWithSubSets from './CheckboxWithSubSets.svelte'
+import { CheckboxWithSubSectionsOptions } from "../../../ts/types";
 
 describe('Checkbox with subsets', () => {
-  const legend = 'Checkbox with subsets'
+  const level1Legend = 'Checkbox with subsets'
   const options = [
     {
       key: 'dyr',
@@ -13,19 +14,19 @@ describe('Checkbox with subsets', () => {
       docCount: 49,
       children: [
         {
-          key: 'produksjonsdyr',
+          key: 'dyr/produksjonsdyr',
           displayName: 'Produksjonsdyr',
           docCount: 38,
           children: []
         },
         {
-          key: 'dyresykdommer',
+          key: 'dyr/dyresykdommer',
           displayName: 'Dyresykdommer',
           docCount: 2,
           children: []
         },
         {
-          key: 'kjaeledyr',
+          key: 'dyr/kjaeledyr',
           displayName: 'Kjæledyr',
           docCount: 1,
           children: []
@@ -38,7 +39,7 @@ describe('Checkbox with subsets', () => {
       docCount: 1,
       children: [
         {
-          key: 'fiskesykdommer',
+          key: 'fisk-og-akvakultur/fiskesykdommer',
           displayName: 'Fiskesykdommer',
           docCount: 1,
           children: []
@@ -51,12 +52,12 @@ describe('Checkbox with subsets', () => {
       docCount: 3,
       children: [
         {
-          key: 'import-av-mat',
+          key: 'mat/import-av-mat',
           displayName: 'Import av mat',
           docCount: 1,
           children: [
             {
-              key: 'kommersiell-import',
+              key: 'mat/import-av-mat/kommersiell-import',
               displayName: 'Kommersiell import',
               docCount: 1,
               children: []
@@ -67,23 +68,12 @@ describe('Checkbox with subsets', () => {
     }
   ]
 
-  const params = {
-    kategori: ['dyr'],
-    underkategori: []
-  }
-
-  const paramsForCheckedTest = {
-    kategori: ['dyr'],
-    underkategori: ['dyr/produksjonsdyr']
-  }
-
   test('Renders list of checkboxes with selected', async () => {
     const {getByText, queryByText} = render(CheckboxWithSubSets, {
-      options,
-      params,
-      legend
+      options: addChecked(['dyr'], options),
+      level1Legend
     })
-    expect(getByText(legend)).toBeInTheDocument()
+    expect(getByText(level1Legend)).toBeInTheDocument()
     expect(getByText(`${options[0].displayName} (${options[0].docCount})`)).toBeInTheDocument()
     expect(getByText(`${options[1].displayName} (${options[1].docCount})`)).toBeInTheDocument()
 
@@ -100,9 +90,8 @@ describe('Checkbox with subsets', () => {
 
   test('Renders subsets of checkboxes', async () => {
     const {getByText, queryByText} = render(CheckboxWithSubSets, {
-      options,
-      params,
-      legend
+      options: addChecked(['dyr'], options),
+      level1Legend
     })
     expect(getByText(`${options[0].displayName} (${options[0].docCount})`)).toBeInTheDocument()
     const animal = getByText(`${options[0].displayName} (${options[0].docCount})`)
@@ -115,11 +104,11 @@ describe('Checkbox with subsets', () => {
   })
 
   test('Check checked checkboxes', async () => {
-    const {getByLabelText} = render(CheckboxWithSubSets, {
-      options,
-      params: paramsForCheckedTest,
-      legend
+    const {getByLabelText, debug} = render(CheckboxWithSubSets, {
+      options:addChecked(['dyr', 'dyr/produksjonsdyr'], options) ,
+      level1Legend
     })
+    debug()
     const mainCategory = getByLabelText(`${options[0].displayName} (${options[0].docCount})`)
     expect(mainCategory).toBeChecked()
 
@@ -131,9 +120,8 @@ describe('Checkbox with subsets', () => {
 
   test('Test that subcategories are unchecked when main category is unchecked', async () => {
     const {getByLabelText, queryByLabelText} = render(CheckboxWithSubSets, {
-      options,
-      params: paramsForCheckedTest,
-      legend
+      options:addChecked(['dyr', 'dyr/produksjonsdyr'], options) ,
+      level1Legend
     })
 
     const mainCategoryCheckbox = getByLabelText(
@@ -173,14 +161,121 @@ describe('Checkbox with subsets', () => {
   test('Test variation: secondary ', async () => {
     // test that class is present in variation secondary
     const {queryAllByText} = render(CheckboxWithSubSets, {
-      options,
-      params,
-      legend,
-      subCategoryLegend : "Ønsker du å velge bare spesifikke tema?",
+      options: addChecked(['dyr'], options)
+      level1Legend,
+      level2Legend : "Ønsker du å velge bare spesifikke tema?",
       variation: 'secondary'
     })
     const legendElement = queryAllByText('Ønsker du å velge bare spesifikke tema?');
     expect(legendElement.length > 0).toEqual(true)
     expect(legendElement[0].parentElement).toHaveClass('checkbox-subsets--secondary')
   })
+
+  test('Checks all ', async () => {
+    const {getByLabelText} = render(CheckboxWithSubSets, {
+      options: [...options],
+      level1Legend,
+      level2Legend : "Ønsker du å velge bare spesifikke tema?",
+      variation: 'secondary',
+      hasCheckAll: true,
+      checkAllLabel: 'Velg alle',
+      checkAllValue: 'dyr'
+    })
+    const selectAll = getByLabelText('Velg alle');
+    expect(selectAll).toBeInTheDocument()
+    const dyr = getByLabelText('Dyr (49)');
+    expect(dyr).not.toBeChecked()
+
+    await fireEvent.change(selectAll, {target: {checked: true}})
+    const dyr1 = getByLabelText('Dyr (49)');
+    expect(dyr1).toBeInTheDocument()
+    expect(dyr1).toBeChecked()
+    const food = getByLabelText('Mat (3)');
+    expect(food).toBeInTheDocument()
+    expect(food).toBeChecked()
+    const fish = getByLabelText('Fisk og akvakultur (1)');
+    expect(fish).toBeInTheDocument()
+    expect(fish).toBeChecked()
+
+    await fireEvent.change(selectAll, {target: {checked: false}})
+    const dyr2 = getByLabelText('Dyr (49)');
+    expect(dyr2).toBeInTheDocument()
+    expect(dyr2).not.toBeChecked()
+    const food1 = getByLabelText('Mat (3)');
+    expect(food1).toBeInTheDocument()
+    expect(food1).not.toBeChecked()
+    const fish1 = getByLabelText('Fisk og akvakultur (1)');
+    expect(fish1).toBeInTheDocument()
+    expect(fish1).not.toBeChecked()
+  })
+
+  test('Checks sub theme', async () => {
+    const {getByLabelText} = render(CheckboxWithSubSets, {
+      options: [...options],
+      level1Legend,
+      level2Legend : "Ønsker du å velge bare spesifikke tema?",
+      variation: 'secondary',
+      hasCheckAll: true,
+      checkAllLabel: 'Velg alle',
+      checkAllValue: 'all'
+    })
+
+    await fireEvent.change(getByLabelText('Velg alle'), {target: {checked: true}})
+    const produksjonsdyr = getByLabelText('Produksjonsdyr (38)')
+    expect(produksjonsdyr).toBeInTheDocument()
+    expect(produksjonsdyr).not.toBeChecked()
+    await fireEvent.change(produksjonsdyr, {target: {checked: true}})
+    const produksjonsdyr1 = getByLabelText('Produksjonsdyr (38)')
+    expect(produksjonsdyr1).toBeChecked()
+    const dyr1 = getByLabelText('Dyr (49)');
+    expect(dyr1).toBeInTheDocument()
+    expect(dyr1).toBeChecked()
+  })
+
+  test('Uncheck theme', async () => {
+    const {getByLabelText, debug} = render(CheckboxWithSubSets, {
+      options: [...options],
+      level1Legend,
+      level2Legend : "Ønsker du å velge bare spesifikke tema?",
+      variation: 'secondary',
+      hasCheckAll: true,
+      checkAllLabel: 'Velg alle',
+      checkAllValue: 'all'
+    })
+
+    await fireEvent.change(getByLabelText('Velg alle'), {target: {checked: true}})
+
+    const dyr1 = getByLabelText('Dyr (49)');
+    expect(dyr1).toBeInTheDocument()
+    expect(dyr1).toBeChecked()
+
+    await fireEvent.change(dyr1, {target: {checked: false}})
+    expect(getByLabelText('Dyr (49)')).not.toBeChecked()
+    expect(getByLabelText('Velg alle')).not.toBeChecked()
+    debug()
+    expect(getByLabelText('Fisk og akvakultur (1)')).toBeChecked()
+    expect(getByLabelText('Mat (3)')).toBeChecked()
+  })
 })
+function addChecked(keys: Array<string>, options: Array<CheckboxWithSubSectionsOptions>) {
+  return options.map((opt) => {
+    const mappedChildren = opt.children?.map((child) => {
+      if(keys.indexOf(child.key) > -1) {
+        return {
+          ...child,
+          checked: true
+        }
+      }
+      return child
+    })
+    const filtered = keys.filter((key) => key === opt.key)
+    if(filtered[0] ) {
+      return {
+        ...opt,
+        checked: true,
+        children: mappedChildren
+      }
+    }
+    return {...opt, children: mappedChildren}
+  })
+}
