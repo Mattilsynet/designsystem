33b596235c53c382e14101fe0ee9486ec75f1092
Amund Lindstøl <alindstol@gmail.com>
2023-08-11 16:01:26 +0200

    Changed storybook css import to use filesystem

--- mt-kit/core/svelte/.storybook/preview.ts
+++ mt-kit/core/svelte/.storybook/preview.ts
@@ -1,10 +1,10 @@
-import type {Preview} from '@storybook/svelte'
-import '@mattilsynet/designsystem-css/dist/app.css'
+import type { Preview } from '@storybook/svelte'
+import '../../css/src/app.scss'
 import '../src/stories/storybook-utils/docs-stories.css'
 
 const preview: Preview = {
   parameters: {
-    actions: {argTypesRegex: '^on[A-Z].*'},
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
