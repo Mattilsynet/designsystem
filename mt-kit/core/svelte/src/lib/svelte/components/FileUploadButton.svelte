dc29d282564e03059e2f19356e441324878a351c
Lisa <halvorsen.lisa@gmail.com>
2023-09-04 11:56:46 +0200

    MTP-1712: add type button
    - to not trigger submit in form

--- mt-kit/core/svelte/src/lib/svelte/components/FileUploadButton.svelte
+++ mt-kit/core/svelte/src/lib/svelte/components/FileUploadButton.svelte
@@ -95,7 +95,7 @@
     fileInputElement.files = dataTransfer.files
   }
 
-  function getFileNames(el: HTMLInputElement) {
+  function getFileNames(el: HTMLInputElement): Array<string> {
     return [...el.files].map(file => file.name)
   }
 
@@ -130,9 +130,9 @@
   class:inclusively-hidden-initial={!onServer}
   aria-describedby={createInputAriaDescribedby(name, error)}
   aria-invalid={!!error}
+  data-testid={name}
   on:change={e => send({ type: 'FILE_SELECTED', fileNames: getFileNames(e.target) })}
-  aria-required={isRequired || undefined}
-/>
+  aria-required={isRequired || undefined} />
 
 {#if !onServer}
   <label class="button button--secondary" style="width: 220px;" for={id}>
@@ -145,21 +145,20 @@
         <li>
           <span class="file-button__file-name">{fileName}</span>
           <button
+            type="button"
             class="button button--search-clear file-button__file-remove"
             on:click={() => send({ type: 'FILE_REMOVE', fileName })}
-          >
+            data-testid={`remove-${fileName}`}>
             <span class="inclusively-hidden">Slett vedlegget: "{fileName}"</span>
             <svg
               aria-hidden="true"
               width="20"
               height="20"
               fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+              xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M10 0C4.47 0 0 4.47 0 10s4.47 10 10 10 10-4.47 10-10S15.53 0 10 0Zm5 13.59L13.59 15 10 11.41 6.41 15 5 13.59 8.59 10 5 6.41 6.41 5 10 8.59 13.59 5 15 6.41 11.41 10 15 13.59Z"
-                fill="#464545"
-              />
+                fill="#464545" />
             </svg>
           </button>
         </li>
