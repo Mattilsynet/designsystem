d7e199beccbdece56fcc0da9c5cdfd5a10e33b3b
Lisa <halvorsen.lisa@gmail.com>
2023-09-04 12:04:58 +0200

    MTP-1712: remove comment

--- mt-kit/core/svelte/setuptest.js
+++ mt-kit/core/svelte/setuptest.js
@@ -2,7 +2,6 @@ import '@testing-library/jest-dom'
 import { vi } from 'vitest'
 
 const DataTransfer = vi.fn(() => {
-  // items: []
   const items = vi.fn()
   items.add = vi.fn()
   const files = vi.fn()
