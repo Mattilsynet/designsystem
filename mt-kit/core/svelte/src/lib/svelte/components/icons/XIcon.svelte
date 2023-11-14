5a3e5ffe99bcd9baab32244222373388aa5cc5a9
Lisa <halvorsen.lisa@gmail.com>
2023-11-14 12:19:45 +0100

    MTP-1914: add x icon

---
+++ mt-kit/core/svelte/src/lib/svelte/components/icons/XIcon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let instanceCounter = 0
+  export let color = '#032C30'
+  export let title: string | undefined = undefined
+  const titleId = `x-icon-${instanceCounter++}`
+</script>
+
+<svg
+  viewBox="0 0 22 22"
+  width="22"
+  height="22"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  {...$$restProps}>
+  {#if title}
+    <title id={titleId}>{title}</title>
+  {/if}
+  <path stroke={color} stroke-width="2" d="M20.6.707.801 20.506M20.506 20.506.707.707" />
+</svg>
