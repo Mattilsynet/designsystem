4669f7a75a7e52a4bd04b11b288f6cff1e3bf71c
Amund Lindstøl <alindstol@gmail.com>
2023-11-27 15:00:27 +0100

    MTP-1862: created Table and TableCol to be able to create tables cleaner

---
+++ mt-kit/core/svelte/src/lib/svelte/components/Table.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  let className = ''
+  export { className as class }
+  export let style = ''
+  export let caption = ''
+  export let headers: string[] = []
+  export let rows: Array<{}> = [{}]
+</script>
+
+{#if caption}
+  <caption class="m-t-m">
+    {caption}
+  </caption>
+{/if}
+<table class="mt-table responsive-table m-t-xxs {className}" id="table-{caption}" style={style}>
+  <thead class="mt-thead">
+    <tr class="mt-tr">
+      {#each headers as header}
+        <slot name="headers" {header} />
+      {/each}
+    </tr>
+  </thead>
+  <tbody class="mt-tbody">
+    {#each rows as row}
+      <slot name="row" {row} />
+    {/each}
+  </tbody>
+</table>
