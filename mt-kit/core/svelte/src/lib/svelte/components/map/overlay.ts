3d75d6bbd7fa9a3c2460ed429606f4579dd70fb3
Lisa <halvorsen.lisa@gmail.com>
2024-01-26 15:30:55 +0100

    MTP-2057: update click popup
    - update styling
    - add close btn label
    - rename btn id

--- mt-kit/core/svelte/src/lib/svelte/components/map/overlay.ts
+++ mt-kit/core/svelte/src/lib/svelte/components/map/overlay.ts
@@ -2,6 +2,9 @@ import type Feature from 'ol/Feature'
 import type Map from 'ol/Map'
 import type { Coordinate } from 'ol/coordinate'
 import { MARKER } from './marker'
+import type { MusselMarker } from '$lib/ts/types'
+
+export const CLICK_POPUP_CLOSE_ID = 'popup-closer'
 
 export function createHoverMarkerContent(feature: Feature): string {
   const features = feature.get('features')
@@ -15,15 +18,18 @@ ${marker.municipality}: ${marker.location}
   return `<ul class="mt-ul list-unstyled ">${items}</ul>`
 }
 
-export function createClickMarkerContent(feature: Feature): string {
-  const marker = feature.get(MARKER)
-  return `<div class="layout-flex layout-flex--center-vertical layout-flex--inline p-lr-xxs background-mt-bg-${marker.status}" style="--gap: var(--spacer-xx-small)">
+export function createClickMarkerContent(closeBtnLabel: string): (feature: Feature) => string {
+  return (feature: Feature) => {
+    const marker: MusselMarker = feature.get(MARKER)
+    return `<div class="layout-flex layout-flex--center-vertical layout-flex--inline p-l-xxxs p-r-0 background-mt-bg-${marker.status}" style="--gap: 0">
 <a href="${marker.href}" class="mt-link text-small">${marker.location}</a>
-<button id="popup-closer"
+<button id="${CLICK_POPUP_CLOSE_ID}"
+      type="button"
       class="mt-button mt-button--search-clear icon--x-before"
-      aria-label="Close me">
+      aria-label="${closeBtnLabel}">
 </button>
       </div>`
+  }
 }
 
 export function setOverlayPosition(map: Map, overlayId: string, coordinate?: Coordinate): void {
