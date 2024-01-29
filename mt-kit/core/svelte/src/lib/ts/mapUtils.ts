f8ff416183d03815c4b7b92a2f388ff677a08e71
Lisa Halvorsen <lisa.halvorsen@mattilsynet.no>
2024-01-29 11:46:28 +0100

    MTP-2080: update zoom and center (#1024)
    - to show all of Norway on initial load on mobil

--- mt-kit/core/svelte/src/lib/ts/mapUtils.ts
+++ mt-kit/core/svelte/src/lib/ts/mapUtils.ts
@@ -1,7 +1,7 @@
 export const ZOOM_CITY = 13
 export const ZOOM_MUNICIPALITY = 10
 export const zoomHalfNorway = 6
-export const ZOOM_NORWAY = 4.3
+export const ZOOM_NORWAY = 4
 export const ZOOM_REGION = 8
 export const MIN_ZOOM_DEFAULT = 3.7
 export const PROJECTION = 'EPSG:3857'
@@ -9,7 +9,7 @@ export const NORGES_GRUNNKART = 'norges_grunnkart'
 export const EUROPA_FORENKLET = 'europa_forenklet'
 export const DEFAULT_MARKER_SCALE = 0.7
 export const DEFAULT_MARKER_OPACITY = 1
-export const DEFAULT_START_COORDINATES = { lat: 65, long: 10 }
+export const DEFAULT_START_COORDINATES = { lat: 65, long: 17 }
 export const DEFAULT_ANIMATION_SPEED = 1000
 export const DEFAULT_CLUSTER_DISTANCE = 30
 export const DEFAULT_CLUSTER_MIN_DISTANCE = 20
