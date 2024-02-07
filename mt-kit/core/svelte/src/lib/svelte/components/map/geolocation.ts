4f9ad3f984df5627c2c3700ced2976eba5c0ebf5
Lisa <halvorsen.lisa@gmail.com>
2024-02-07 11:41:54 +0100

    MTP-2084: add setup for geolocation

---
+++ mt-kit/core/svelte/src/lib/svelte/components/map/geolocation.ts
@@ -0,0 +1,79 @@
+import { Geolocation, type Map, type View } from 'ol'
+import CircleStyle from 'ol/style/Circle'
+import { Fill, Stroke, Style } from 'ol/style'
+import Feature from 'ol/Feature'
+import { type Geometry, Point } from 'ol/geom'
+import VectorLayer from 'ol/layer/Vector'
+import VectorSource from 'ol/source/Vector'
+import { LAYER_ID } from './layer-utils'
+
+export const GEOLOCATION_LAYER_ID = 'geolocationLayer'
+
+export function setUpGeolocation(map: Map): {
+  layer: VectorLayer<VectorSource<Feature<Geometry>>>
+  geolocation: Geolocation
+} {
+  const view = map.getView()
+  const geolocation = getGeoLocation(view)
+  geolocation.setTracking(false)
+  const feature = addMyLocationFeature()
+  const coordinates = geolocation.getPosition()
+  feature.setGeometry(coordinates ? new Point(coordinates) : undefined)
+  addGeolocationListeners(geolocation, feature)
+
+  const layer = new VectorLayer({
+    source: new VectorSource({
+      features: [feature]
+    }),
+    visible: true
+  })
+  layer.set(LAYER_ID, GEOLOCATION_LAYER_ID)
+  return {
+    geolocation,
+    layer
+  }
+}
+export function getGeoLocation(view: View): Geolocation {
+  return new Geolocation({
+    trackingOptions: {
+      enableHighAccuracy: true
+    },
+    projection: view.getProjection()
+  })
+}
+
+export function addGeolocationListeners(geolocation: Geolocation, positionFeature: Feature): void {
+  geolocation.on('change', function () {
+    const coordinates = geolocation.getPosition()
+    positionFeature.setGeometry(coordinates ? new Point(coordinates) : undefined)
+  })
+  geolocation.on('change:position', function () {
+    const coordinates = geolocation.getPosition()
+    positionFeature.setGeometry(coordinates ? new Point(coordinates) : undefined)
+  })
+  geolocation.on('error', function (error) {
+    console.log('on error', error)
+    //     const info = document.getElementById('info')
+    //     info.innerHTML = error.message
+    //     info.style.display = ''
+  })
+}
+
+export function addMyLocationFeature(): Feature {
+  const positionFeature = new Feature()
+  positionFeature.setStyle(
+    new Style({
+      image: new CircleStyle({
+        radius: 8,
+        fill: new Fill({
+          color: '#3399CC'
+        }),
+        stroke: new Stroke({
+          color: '#fff',
+          width: 2
+        })
+      })
+    })
+  )
+  return positionFeature
+}
