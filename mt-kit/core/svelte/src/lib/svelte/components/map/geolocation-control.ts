53c771916178fb9a721497c474f1092fa308490b
Lisa <halvorsen.lisa@gmail.com>
2024-02-07 11:40:42 +0100

    MTP-2084: add setup for geolocation control

---
+++ mt-kit/core/svelte/src/lib/svelte/components/map/geolocation-control.ts
@@ -0,0 +1,68 @@
+import { Control } from 'ol/control'
+import { type Map, type Geolocation } from 'ol'
+import { markers } from '../../../ts/markers'
+import { GEOLOCATION_LAYER_ID, setUpGeolocation } from './geolocation'
+import { LAYER_ID } from './layer-utils'
+import type { MTGeolocationOptions } from '$lib/ts'
+
+export function createGeolocationControl(
+  map: Map,
+  geolocationOptions: MTGeolocationOptions
+): Control {
+  const { layer, geolocation } = setUpGeolocation(map)
+  map.addLayer(layer)
+
+  const { input, label } = createTrackerToggle(map, geolocation, geolocationOptions)
+  const element = createTrackerToggleWrapper()
+  element.appendChild(input)
+  element.appendChild(label)
+
+  const control = new Control({ element })
+  control.setMap(map)
+
+  return control
+}
+
+function handleTracking(map: Map, geolocation: Geolocation): (e: Event) => void {
+  return (event: Event): void => {
+    const layer = map
+      .getLayers()
+      .getArray()
+      .find(layer => layer.get(LAYER_ID) === GEOLOCATION_LAYER_ID)
+    if (event.target && layer) {
+      geolocation.setTracking(event.target.checked)
+      layer.setVisible(event.target.checked)
+    }
+  }
+}
+
+function createTrackerToggle(
+  map: Map,
+  geolocation: Geolocation,
+  geolocationOptions: MTGeolocationOptions
+): { input: HTMLInputElement; label: HTMLLabelElement } {
+  const locationTrackerId = 'locationTracker'
+  const label = document.createElement('label')
+  label.innerHTML = markers.icon.geolocation
+  label.htmlFor = locationTrackerId
+  label.className = 'mt-label map-toggle-label'
+  label.title = geolocationOptions.label
+
+  const input = document.createElement('input')
+  input.id = locationTrackerId
+  input.type = 'checkbox'
+  input.className = 'mt-input map-toggle-input'
+  input.addEventListener('change', handleTracking(map, geolocation))
+
+  return {
+    input,
+    label
+  }
+}
+
+function createTrackerToggleWrapper(): HTMLDivElement {
+  const element = document.createElement('div')
+  element.id = 'geolocationTracker'
+  element.className = 'geolocation-button ol-unselectable ol-control'
+  return element
+}
