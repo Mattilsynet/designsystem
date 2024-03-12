f68495000da6cac4c08a95bd8745d8bf75c9555a
Lisa <halvorsen.lisa@gmail.com>
2024-03-12 08:27:58 +0100

    MTP-2171: Refac move pagination story to separate story

--- mt-kit/core/svelte/src/stories/Links.stories.svelte
+++ mt-kit/core/svelte/src/stories/Links.stories.svelte
@@ -1,19 +1,7 @@
 <script lang="ts">
   import { Meta, Story } from '@storybook/addon-svelte-csf'
   import Link from '../lib/svelte/components/Link.svelte'
-  import Disclosure from '../lib/svelte/components/Disclosure.svelte'
-  import { action } from '@storybook/addon-actions'
-  import ChapterNavigation from '../lib/svelte/components/ChapterNavigation.svelte'
   import { wrapInShadowDom } from './storybook-utils/utils'
-
-  const chapterChangeAction = action('chapterChange')
-  let currentChapterNumber = 1
-
-  function chapterChange(e) {
-    e.preventDefault()
-    chapterChangeAction(e)
-    currentChapterNumber = e.detail.index
-  }
 
   function getColSpanClass(twoColumns, index) {
     if (!twoColumns) {
@@ -46,15 +34,7 @@
         text: 'Kritikk&shy;verdige forhold på arbeids&shy;plassen hvor lenken går over flere linjer og samtidig skal ha animasjon'
       }
     ],
-    chapters: [
-      { index: 0, url: '', heading: 'Formål' },
-      { index: 1, url: '', heading: 'Virkeområde' },
-      { index: 2, url: '', heading: 'Definisjoner av dyr og dyrehold' },
-      { index: 3, url: '', heading: 'Forurensning av vann' },
-      { index: 4, url: '', heading: 'Kapittel 4' },
-      { index: 5, url: '', heading: 'Kapittel 5' },
-      { index: 6, url: '', heading: 'Kapittel 6' }
-    ],
+
     smallLinks: [
       {
         url: '#',
@@ -81,7 +61,7 @@
         text: 'Kritikkverdige forhold på arbeids&shy;plassen'
       }
     ],
-    showChapterNumber: true,
+
     secondary: 'Avbryt',
     inText: 'mattilsynet',
     twoColumns: false,
@@ -92,9 +72,8 @@
     primary: { control: 'text' },
     inText: { control: 'text' },
     cards: { control: 'array' },
-    chapters: { control: 'array' },
     smallLinks: { control: 'array' },
-    showChapterNumber: { control: 'boolean' },
+
     secondary: { control: 'string' },
     disabled: { control: 'boolean' },
     disableCss: { control: 'boolean' }
@@ -144,17 +123,6 @@
       <div class="wrapper">
         <Link href="#" class="inline-flex" linkText={args.cards[5].text} />
       </div>
-    </article>
-    <article>
-      <h2 class="mt-h2">Neste og forrig lenker (paginering)</h2>
-      <ChapterNavigation
-        showChapterNumber={args.showChapterNumber}
-        chapters={args.chapters}
-        currentChapterIndex={currentChapterNumber}
-        on:chapterChange={chapterChange}
-        nextText="Neste"
-        previousText="Forrige"
-        class="chapter-navigation--bottom" />
     </article>
   </div>
 </Story>
