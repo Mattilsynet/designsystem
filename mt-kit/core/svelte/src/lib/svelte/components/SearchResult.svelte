eb119ff2cd4ed6e0a8f6895a3532d2c1ae40a7df
Amund Lindstøl <alindstol@gmail.com>
2024-03-21 13:30:16 +0100

    MTP-2155: created a search result component

---
+++ mt-kit/core/svelte/src/lib/svelte/components/SearchResult.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { mapRelExternal } from '../../ts/utils'
+  import HeadingLevel from '../components/HeadingLevel.svelte'
+
+  type SearchResult = {
+    title?: string
+    url?: string
+    text?: string
+    breadcrumbs?: string[]
+  }
+
+  export let searchResult: Array<SearchResult> = []
+  export let headingClass = 'mt-h3'
+  export let headerTag: 'h2' | 'h3' = 'h2'
+</script>
+
+{#each searchResult as result, index}
+  <a
+    href={result.url}
+    rel={mapRelExternal(result.url)}
+    class="mt-link col-1-span-12 layout-flex layout-flex-col border-radius search-result">
+    <HeadingLevel class="heading {headingClass}" headingLevel={+headerTag.charAt(1)}>
+      {@html result.title}
+    </HeadingLevel>
+    {#if result.text}
+      <div class="text">
+        {@html result.text}
+      </div>
+    {/if}
+    {#if result.breadcrumbs?.length > 0}
+      <div>
+        {#each result.breadcrumbs as breadcrumb}
+          <span class="breadcrumb">
+            {@html breadcrumb}
+          </span>
+        {/each}
+      </div>
+    {/if}
+  </a>
+{/each}
