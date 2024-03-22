f82a8535a6da94f918412186c88e8dc1f9f4c053
Amund Lindstøl <alindstol@gmail.com>
2024-03-22 12:29:08 +0100

    MTP-2155 hele treffet er klikkbart (#1069)
    * MTP-2155: created a search result component
    
    * MTP-2155: qa
    
    * MTP-2155: add searchResult component to exports
    
    * MTP-2155: reduce spacing between breadcrumbs
    
    * MTP-2155: qa

---
+++ mt-kit/core/svelte/src/lib/svelte/components/SearchResult.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { mapRelExternal } from '../../ts/utils'
+  import HeadingLevel from '../components/HeadingLevel.svelte'
+  import type { SearchResult } from '$lib/ts'
+
+  export let searchResult: Array<SearchResult> = []
+  export let headingClass = 'mt-h3'
+  export let headerTag: 'h2' | 'h3' = 'h2'
+  export let breadCrumbAriaLabel = 'Brødsmulesti'
+</script>
+
+{#each searchResult as result}
+  <a
+    href={result.url}
+    rel={mapRelExternal(result.url)}
+    class="mt-link col-1-span-12 layout-flex layout-flex-col border-radius search-result">
+    <HeadingLevel class="heading {headingClass}" headingLevel={+headerTag.charAt(1)}>
+      {@html result.title}
+    </HeadingLevel>
+    {#if result.text}
+      <div class="text">
+        {@html result.text}
+      </div>
+    {/if}
+    {#if result.breadcrumbs?.length > 0}
+      <div aria-label={breadCrumbAriaLabel}>
+        {#each result.breadcrumbs as breadcrumb}
+          <span class="breadcrumb">
+            {@html breadcrumb}
+          </span>
+        {/each}
+      </div>
+    {/if}
+  </a>
+{/each}
