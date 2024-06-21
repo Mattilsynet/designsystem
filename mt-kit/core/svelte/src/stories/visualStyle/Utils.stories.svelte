9f8051d421ebcae25fdfa464ad28ca4113a5e9dd
Lisa Halvorsen <lisa.halvorsen@mattilsynet.no>
2024-06-21 08:15:45 +0200

    MTP-2369 oppdater styling pa oppsummering (#1104)
    * MTP-2369: var for border-radius-large, update list+menu border color to furu
    
    * MTP-2369: add rule for button under input
    
    - for form summary submit button
    
    * MTP-2369: remove margin above page-summaries
    
    - using padding instead to have a the same color on summary parts
    
    * MTP-2369: add util for p-s and box-border-radius-br-l
    
    - for use on form summary page
    
    * MTP-2369: add story to display box-border-radius

---
+++ mt-kit/core/svelte/src/stories/visualStyle/Utils.stories.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { Meta, Story } from '@storybook/addon-svelte-csf'
+  import { wrapInShadowDom } from '../storybook-utils/utils'
+</script>
+
+<Meta
+  title="Visual Style/Utils"
+  args={{
+    disableCss: false
+  }}
+  argTypes={{
+    disableCss: { control: 'boolean' }
+  }} />
+<Story name="All" let:disableCss>
+  <div use:wrapInShadowDom={disableCss} class="layout-flex-col">
+    <h1>Utils</h1>
+    <h2>Box border radius</h2>
+    <article class="background-mt-white box-border-radius-br-l p-s">
+      <h3 class="border-b-secondary m-b-xxs">This is an article</h3>
+      <p>class:box-border-radius</p>
+      <p>
+        class proin eirmod aperiri libris ubique dui vestibulum mediocritatem imperdiet saepe
+        dapibus euripidis qui potenti omnesque sem vix vituperatoribus non vix dictum petentium
+        bibendum mauris deseruisse antiopam vivamus eloquentiam inceptos senserit mediocritatem
+        lacus mentitum consectetur natoque duis massa iriure conubia vim feugait simul integer amet
+        nobis persecuti phasellus finibus dictumst posuere diam menandri fames fuisset facilisis
+        ocurreret sem prodesset dicunt justo homero equidem magnis dolores intellegat hendrerit sumo
+        habeo elaboraret accusata placerat omnesque sagittis duo veritus option felis aperiri velit
+        aliquip inimicus quidam mediocritatem delenit eloquentiam definitionem suspendisse id mauris
+        dolor utinam utamur alia eripuit quod gloriatur solet adversarium referrentur
+      </p>
+    </article>
+    <article class="background-mt-white box-border-radius-br-l p-s m-t-xs">
+      <h3 class="border-b-secondary m-b-xxs">This is an article</h3>
+      <p>class:box-border-radius</p>
+    </article>
+  </div>
+</Story>
