b5591693822a39b9c95947e91628b0a18c66e879
Petter Iversen <69626923+petterkoding@users.noreply.github.com>
2024-07-10 08:42:00 +0200

    MTP-2407 use local scoped vars, fix border radius for normal version

--- mt-kit/core/svelte/src/lib/svelte/content/HighlightedContentLink.svelte
+++ mt-kit/core/svelte/src/lib/svelte/content/HighlightedContentLink.svelte
@@ -21,7 +21,7 @@
   <a
     href={url}
     rel={mapRelExternal(url)}
-    class="mt-link col-1-span-12 layout-flex layout-flex-col border-radius box-border-radius-br-l highlighted-content highlighted-content--{displayType}"
+    class="mt-link col-1-span-12 layout-flex layout-flex-col box-border-radius-br-l highlighted-content highlighted-content--{displayType}"
     data-testid="highlighted-content-link">
     <HeadingLevel class="heading {headingClass}" headingLevel={+headerTag.charAt(1)}>
       {title}
@@ -32,7 +32,7 @@
   <a
     href={url}
     rel={mapRelExternal(url)}
-    class="mt-link highlighted-content highlighted-content--{displayType} {className}">
+    class="mt-link box-border-radius-br-l highlighted-content highlighted-content--{displayType} {className}">
     <span class="col-1-span-5 layout-flex layout-flex-col highlighted-text">
       {#if shortTitle}
         <HeadingLevel class="heading {headingClass}" headingLevel={+headerTag.charAt(1)}>
@@ -49,7 +49,7 @@
   <a
     href={url}
     rel={mapRelExternal(url)}
-    class="mt-link highlighted-content {className}"
+    class="mt-link highlighted-content border-radius {className}"
     data-testid="highlighted-content-link">
     {#if image && image.src}
       <img src={image.src} alt={image.alt} class="mt-img" />
