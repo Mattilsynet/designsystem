a9e6f4a239a866a50b493794e207934624136f56
Petter Iversen <69626923+petterkoding@users.noreply.github.com>
2024-08-07 10:25:24 +0200

    remove unused code in tests

--- mt-kit/core/svelte/src/lib/svelte/components/Link.test.ts
+++ mt-kit/core/svelte/src/lib/svelte/components/Link.test.ts
@@ -37,26 +37,4 @@ describe('Link', () => {
     expect(link).toBeInTheDocument()
     expect(link.getAttribute('rel')).toEqual(null)
   })
-
-  test('Renders with is-external class on external link', () => {
-    const { getByText } = render(Link, {
-      ...componentOptions,
-      href: 'https://www.nav.no/'
-    })
-    const link = getByText(/This is the link text/i)
-    expect(link).toBeInTheDocument()
-    expect(link.getAttribute('rel')).toEqual('external')
-    expect(link.classList.contains('is-external')).toBe(true)
-  })
-
-  test('Renders without is-external class when its relative', () => {
-    const { getByText } = render(Link, {
-      ...componentOptions,
-      href: '/dyr/dyresykdommer'
-    })
-    const link = getByText(/This is the link text/i)
-    expect(link).toBeInTheDocument()
-    expect(link.getAttribute('rel')).toEqual(null)
-    expect(link.classList.contains('is-external')).toBe(false)
-  })
 })
