a15fbd38a27becf3e212f92a86d9ad956236b5f1
Petter Iversen <69626923+petterkoding@users.noreply.github.com>
2024-08-16 17:11:01 +0200

    change fallback, update else with empty string

--- mt-kit/core/svelte/src/lib/svelte/content/MenuPointsIcons.svelte
+++ mt-kit/core/svelte/src/lib/svelte/content/MenuPointsIcons.svelte
@@ -19,7 +19,7 @@
         href={menuPoint.url}
         rel={mapRelExternal(menuPoint.url)}
         class="menu-point border-radius p-xs"
-        aria-describedby={toKebabCase(menuPoint?.keywords ? menuPoint.keywords : menuPoint.text)}
+        aria-describedby={toKebabCase(menuPoint?.keywords ? menuPoint.keywords : '')}
         data-testid="menupoints-link">
         {#if menuPoint.iconResource}
           <div class="svg-wrapper" aria-hidden="true">
