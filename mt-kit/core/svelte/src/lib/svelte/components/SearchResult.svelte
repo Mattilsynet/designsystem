d68c2e70f84f8c13ef268804a70325faff1e345a
Petter Iversen <69626923+petterkoding@users.noreply.github.com>
2024-09-25 17:30:30 +0200

    use fds spacings

--- mt-kit/core/svelte/src/lib/svelte/components/SearchResult.svelte
+++ mt-kit/core/svelte/src/lib/svelte/components/SearchResult.svelte
@@ -25,7 +25,7 @@
     {#if result.breadcrumbs?.length > 0}
       <div
         aria-label={breadCrumbAriaLabel}
-        style="--gap: var(--spacer-xxx-small)"
+        style="--gap: var(--fds-spacing-2)"
         class="layout-flex layout-flex--center-vertical">
         {#each result.breadcrumbs as breadcrumb}
           <span class="breadcrumb">
