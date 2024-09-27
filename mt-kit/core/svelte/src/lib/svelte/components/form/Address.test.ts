83a41a13e69782ac0b51083439335b200056eefe
Lisa <halvorsen.lisa@gmail.com>
2024-09-27 08:19:54 +0200

    MTP-2488: add user interaction test

--- mt-kit/core/svelte/src/lib/svelte/components/form/Address.test.ts
+++ mt-kit/core/svelte/src/lib/svelte/components/form/Address.test.ts
@@ -1,28 +1,111 @@
-import { render } from '@testing-library/svelte'
+import { render, screen, fireEvent } from '@testing-library/svelte'
+import userEvent from '@testing-library/user-event'
 import Address from './Address.svelte'
+import { http, HttpResponse } from 'msw'
+import { setupServer } from 'msw/node'
+
+const addressUrl = 'https://ws.geonorge.no/adresser/v1/sok'
+const server = setupServer(
+  http.get(addressUrl, () => {
+    return HttpResponse.json({
+      adresser: [
+        { adressetekst: 'Testveien 123', postnummer: '0190', poststed: 'Oslo' },
+        { adressetekst: 'Testveien 321', postnummer: '0190', poststed: 'Oslo' }
+      ]
+    })
+  })
+)
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 describe('Addresss', () => {
-  const helpText = 'This is the helptext'
-  const options = [
-    { text: 'Ja', value: 'yes' },
-    { text: 'Nei', value: 'no' }
-  ]
-  const properties = {
-    validationRequired: {
-      errorMessage: 'Error'
-    }
-  }
   const props = {
-    nameAddress: 'ownerAddress',
-    nameZip: 'ownerZip'
+    streetLabel: 'Søk i gateadresse',
+    streetName: 'owner-street',
+    streetIsRequired: true,
+    streetError: undefined,
+    streetHelpText: 'Hjelpetekst',
+    streetInputClass: '',
+    streetFallbackLabel: 'Gateadressse',
+    postalCodeLabel: 'Postnummer',
+    postalCodeName: 'owner-postal-code',
+    postalCodeIsRequired: true,
+    postalCodeError: undefined,
+    postalCodeHelpText: 'Hjelpetekst',
+    postalCodeInputClass: '',
+    loadJs: true,
+    hiddenErrorText: 'Feilmelding: ',
+    formInProgressAriaLabel: 'Søker...'
   }
-  test('Renders', () => {
-    const { getByLabelText, getByText, getByRole } = render(Address, props)
-    expect(getByText(helpText)).toBeInTheDocument()
-    expect(getByLabelText(options[0].text)).toBeInTheDocument()
-    expect(getByLabelText(options[1].text)).toBeInTheDocument()
-    const fieldSet = getByRole('checkbox', { name: 'Ja' })
-    expect(fieldSet?.getAttribute('aria-required')).toEqual('true')
-    expect(fieldSet?.getAttribute('aria-describedby')).toEqual('name-hint')
+  test('Renders - typing', async () => {
+    const user = userEvent.setup()
+    const { getByLabelText, queryByLabelText } = render(Address, props)
+    const street = getByLabelText('Søk i gateadresse')
+    expect(street).toBeInTheDocument()
+    expect(queryByLabelText('Postnummer')).not.toBeInTheDocument()
+    await user.type(street, 'Test')
+    await screen.findByLabelText(/Søker/)
+    await screen.findByText(/Testveien 123/)
+    await fireEvent.click(screen.getByText(/Testveien 123/))
+    expect(screen.getByTestId('hidden-street')).toHaveValue('Testveien 123')
+    expect(screen.getByTestId('hidden-postal-code')).toHaveValue('0190')
+  })
+
+  test('Renders - no results', async () => {
+    server.use(
+      http.get(addressUrl, () => {
+        return HttpResponse.json({
+          adresser: []
+        })
+      })
+    )
+    const user = userEvent.setup()
+    const { getByLabelText, queryByLabelText } = render(Address, props)
+    const street = getByLabelText('Søk i gateadresse')
+    expect(street).toBeInTheDocument()
+    expect(queryByLabelText('Postnummer')).not.toBeInTheDocument()
+    await user.type(street, 'Test')
+    await screen.findByText(/Ingen resultater/)
+  })
+
+  test('Renders - fetch fails', async () => {
+    server.use(
+      http.get(addressUrl, () => {
+        return new HttpResponse(null, { status: 500 })
+      })
+    )
+    const user = userEvent.setup()
+    const { getByLabelText, queryByLabelText, getByRole } = render(Address, props)
+    const street = getByLabelText('Søk i gateadresse')
+    expect(street).toBeInTheDocument()
+    expect(queryByLabelText('Postnummer')).not.toBeInTheDocument()
+    await user.type(street, 'Test')
+    await screen.findByText(/Skriv inn manuelt under/)
+
+    const fallbackStreet = getByLabelText('Gateadressse')
+    expect(fallbackStreet).toBeInTheDocument()
+    const postalCode = getByLabelText('Postnummer')
+    expect(postalCode).toBeInTheDocument()
+
+    const searchStreet = getByRole('combobox')
+    await fireEvent.focus(searchStreet)
+    await user.type(street, 'T')
+    const errorNotDisplayed = await screen.queryByText(/Skriv inn manuelt under/)
+    expect(errorNotDisplayed).not.toBeInTheDocument()
+    await user.type(street, 'Test')
+    const errorDisplayed = await screen.findByText(/Skriv inn manuelt under/)
+    expect(errorDisplayed).toBeInTheDocument()
+  })
+
+  test('Renders without JS', () => {
+    const { getByLabelText } = render(Address, {
+      ...props,
+      loadJs: false
+    })
+    expect(getByLabelText('Søk i gateadresse')).toBeInTheDocument()
+    expect(getByLabelText('Postnummer')).toBeInTheDocument()
   })
 })
