34dfa262d29303a8caa09f9aa5191b3249739907
Lisa <halvorsen.lisa@gmail.com>
2024-11-12 13:17:38 +0100

    MTP-2654: remove only selected
    - to fix remove all

--- mt-kit/core/svelte/src/lib/svelte/components/form/FileUpload.svelte
+++ mt-kit/core/svelte/src/lib/svelte/components/form/FileUpload.svelte
@@ -4,7 +4,7 @@
   import type { ErrorDetail } from '../../../ts/types'
   import Label from './Label.svelte'
 
-  export let value
+  export let value: undefined | string | Array<string>
   export let name: string
   export let label: string
   export let buttonText: string
@@ -23,14 +23,28 @@
   let nameInput: HTMLInputElement
 
   function onRemoveFile(e: CustomEvent): void {
-    //update file. This handles one file upload to mats
+    const fileNames: Array<string> = nameInput.value.split(',')
     uuidInput.value = ''
-    value = undefined
+    value = removeValueByFileName(value, fileNames, e.detail.fileName)
 
     // update filename. This handles multiple files
-    const values: Array<string> = nameInput.value.split(',')
-    nameInput.value = values.filter(v => v !== e.detail.fileName).join(',')
+    nameInput.value = fileNames.filter(v => v !== e.detail.fileName).join(',')
     fileName = filterFileName(fileName, e.detail.fileName)
+  }
+
+  function removeValueByFileName(
+    value: undefined | string | Array<string>,
+    fileNames: Array<string>,
+    removeFileName: string
+  ): undefined | Array<string> {
+    const index = fileNames.indexOf(removeFileName)
+    if (value === undefined) {
+      return undefined
+    } else if (typeof value === 'string') {
+      return undefined
+    } else {
+      return value.filter((_, i) => i !== index)
+    }
   }
 
   function filterFileName(
@@ -57,7 +71,13 @@
   <InputError message={error.message} key={fileInputName} {hiddenErrorText} />
 {/if}
 
-<input type="hidden" class="mt-input" bind:this={uuidInput} {name} value={value || ''} data-testid={name} />
+<input
+  type="hidden"
+  class="mt-input"
+  bind:this={uuidInput}
+  {name}
+  value={value || ''}
+  data-testid={name} />
 <input
   type="hidden"
   class="mt-input"
