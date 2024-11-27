f95671e560fb3c80b448451f49c49c7ac89d0c3e
Amund Lindstøl <alindstol@gmail.com>
2024-11-27 08:30:17 +0100

    MTP-2764: add $lib to vite test alias

--- mt-kit/core/svelte/vite.config.ts
+++ mt-kit/core/svelte/vite.config.ts
@@ -2,6 +2,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { svelteTesting } from '@testing-library/svelte/vite'
 import { defineConfig } from 'vitest/config'
 import { searchForWorkspaceRoot } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [svelte(), svelteTesting()],
@@ -10,7 +11,10 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./setuptest.js'],
     include: ['src/**/*.{test,spec}.{js,ts}'],
-    alias: [{ find: /^svelte$/, replacement: 'svelte/internal' }] //https://github.com/vitest-dev/vitest/issues/2834
+    alias: [
+      { find: /^svelte$/, replacement: 'svelte/internal' }, //https://github.com/vitest-dev/vitest/issues/2834
+      { find: '$lib', replacement: resolve(__dirname, './src/lib') }
+    ]
   },
   server: {
     fs: {
