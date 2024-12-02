c2b5bd50ff60b0bdc6ef78502e4076d880357edf
Lisa <halvorsen.lisa@gmail.com>
2024-12-02 13:14:20 +0100

    MTP-2793: update config

--- mt-kit/core/svelte/eslint.config.mjs
+++ mt-kit/core/svelte/eslint.config.mjs
@@ -1,60 +1,43 @@
-import tseslint from 'typescript-eslint'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import eslintPluginSvelte from 'eslint-plugin-svelte';
-import * as svelteParser from 'svelte-eslint-parser';
-import eslintPluginStorybook from 'eslint-plugin-storybook';
-import svelteConfig from './svelte.config.js'
-import globals from 'globals'
+import tseslint from "typescript-eslint"
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended"
+import eslintPluginSvelte from "eslint-plugin-svelte"
+import * as svelteParser from "svelte-eslint-parser"
+import eslintPluginStorybook from "eslint-plugin-storybook"
+import importPlugin from 'eslint-plugin-import'
+import svelteConfig from "./svelte.config.js"
+import globals from "globals"
 
 export default [
-  ...tseslint.configs.recommended,
-  ...eslintPluginSvelte.configs.recommended,
-  eslintPluginStorybook.configs.recommended,
-  eslintConfigPrettier,
-  {
-    files: ['**/*.svelte', '*.svelte'],
+  ...tseslint.config({
     languageOptions: {
-      parser:svelteParser,
+      parserOptions: {
+        ecmaVersion: 2019,
+        project: "./tsconfig.json"
+        , extraFileExtensions: [".svelte"]
+      }
+    }
+  }),
+  ...importPlugin.flatConfigs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginSvelte.configs["flat/recommended"],
+  ...eslintPluginStorybook.configs["flat/recommended"],
+  eslintPluginPrettier,
+  {
+    files: ["**/*.svelte", "*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
       parserOptions: {
         svelteConfig
       }
     }
   },
   {
-    files: ['**/*.ts'],
-    ignores: ['dist/**/*.*'],
+    files: ["**/*.ts"],
+    ignores: ["dist/**/*.*", "mt-kit/core/react"],
     languageOptions: {
       globals: {
         ...globals.browser
       }
     }
   }
-]
-// export default tseslint.config(
-//   tseslint.configs.eslintRecommended,
-//   eslintConfigPrettier,
-//   {
-//     languageOptions: {
-//       parserOptions: {
-//         ecmaFeatures: {
-//           jsx: true,
-//         },
-//       },
-//       globals: {
-//         ...globals.browser
-//       }
-//     },
-//
-//     ignores: ['dist/**/*.*'],
-//     files: ['**/*.ts', '**/*.svelte'],
-//
-//     plugins: {
-//     },
-//
-//     rules: {
-//       // https://eslint.org/docs/rules
-//       'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-//       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-//     },
-//   },
-// )
+];
