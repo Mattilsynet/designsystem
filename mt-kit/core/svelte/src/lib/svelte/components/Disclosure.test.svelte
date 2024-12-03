228ef3ce1a70e2a0d9bd90b29ebd381ee2f32e0d
Lisa <halvorsen.lisa@gmail.com>
2024-12-03 08:58:34 +0100

    MTP-2637: remove xstate for disclosure
    - change default loadJs

---
+++ mt-kit/core/svelte/src/lib/svelte/components/Disclosure.test.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Disclosure from './disclosure.svelte'
+</script>
+
+<Disclosure {...$$props}>
+  <p>This is the disclosure panel text</p>
+</Disclosure>
--- mt-kit/core/svelte/src/lib/svelte/components/Disclosure.test.ts
+++ mt-kit/core/svelte/src/lib/svelte/components/Disclosure.test.ts
@@ -1,27 +1,30 @@
 import { fireEvent, render } from '@testing-library/svelte'
-import Disclosure from './Disclosure.svelte'
+import DisclosureMock from './Disclosure.test.svelte'
 
 describe('Disclosure', () => {
   const componentOptions = {
-    title: 'Varsle om piggtrådgjerder'
+    title: 'Varsle om piggtrådgjerder',
+    loadJs: true
   }
 
   test('Renders', () => {
-    const { getByText } = render(Disclosure, { props: componentOptions })
+    const { getByText, queryByText } = render(DisclosureMock, { props: componentOptions })
     const byText = getByText('Varsle om piggtrådgjerder')
     expect(byText.parentElement.getAttribute('aria-expanded')).toEqual('false')
     const disclosure = document.querySelector('.disclosure-bordered')
     expect(disclosure).toBeInTheDocument()
+    const panel = queryByText('This is the disclosure panel text')
+    expect(panel).not.toBeInTheDocument()
   })
 
   test('Renders - links', () => {
-    render(Disclosure, { props: { ...componentOptions, theme: 'links' } })
+    render(DisclosureMock, { props: { ...componentOptions, theme: 'links' } })
     const disclosure = document.querySelector('.disclosure-links')
     expect(disclosure).toBeInTheDocument()
   })
 
   test('Renders - no-border', () => {
-    render(Disclosure, {
+    render(DisclosureMock, {
       props: { ...componentOptions, theme: 'no-border', class: 'background-mt-white' }
     })
     const disclosure = document.querySelector('.disclosure-no-border')
@@ -30,29 +33,48 @@ describe('Disclosure', () => {
     expect(white).toBeInTheDocument()
   })
 
+  test('Renders - no.js', () => {
+    const { getByText } = render(DisclosureMock, {
+      props: { ...componentOptions, loadJs: false }
+    })
+    const byText = getByText('Varsle om piggtrådgjerder')
+    expect(byText).toBeInTheDocument()
+    const panel = getByText('This is the disclosure panel text')
+    expect(panel).toBeInTheDocument()
+  })
+
   test('Renders - start open', () => {
-    const { getAllByText } = render(Disclosure, { props: { ...componentOptions, startOpen: true } })
+    const { getAllByText, getByText } = render(DisclosureMock, {
+      props: { ...componentOptions, startOpen: true }
+    })
     const byText = getAllByText('Varsle om piggtrådgjerder')
     expect(byText.length > 0).toEqual(true)
     const btn = byText.find(el => {
       return el.parentElement.tagName === 'BUTTON'
     })
     expect(btn.parentElement.getAttribute('aria-expanded')).toEqual('true')
+    const panel = getByText('This is the disclosure panel text')
+    expect(panel).toBeInTheDocument()
   })
 
   test('Render correct heading level - h2', async () => {
-    const { getAllByText } = render(Disclosure, {
+    const { getAllByText, queryByText, getByText } = render(DisclosureMock, {
       props: { ...componentOptions, headerTag: 'h2' }
     })
     const heading = getAllByText('Varsle om piggtrådgjerder')
+    const panel = queryByText('This is the disclosure panel text')
+    expect(panel).not.toBeInTheDocument()
+
     await fireEvent.click(heading[0])
 
     const all = getAllByText('Varsle om piggtrådgjerder')
     expect(all[1].tagName).toEqual('H2')
+    const panelOpen = getByText('This is the disclosure panel text')
+    expect(panelOpen).toBeInTheDocument()
   })
 
   test('Render correct heading level - h3', async () => {
-    const { getAllByText } = render(Disclosure, {
+    const { getAllByText } = render(DisclosureMock, {
       props: { ...componentOptions, headerTag: 'h3' }
     })
     const heading = getAllByText('Varsle om piggtrådgjerder')
@@ -63,7 +85,7 @@ describe('Disclosure', () => {
   })
 
   test('Render correct heading level - h4', async () => {
-    const { getAllByText } = render(Disclosure, {
+    const { getAllByText } = render(DisclosureMock, {
       props: { ...componentOptions, headerTag: 'h4' }
     })
     const heading = getAllByText('Varsle om piggtrådgjerder')
