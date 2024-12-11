aba245ec2bac586c2860436599be92dd13f4feb2
Amund Lindstøl <alindstol@gmail.com>
2024-12-11 14:34:36 +0100

    MTP-2803: fixed HighlightedContentLink stories

--- mt-kit/core/svelte/src/stories/nettlosninger/HighlightedContentLink.stories.svelte
+++ mt-kit/core/svelte/src/stories/nettlosninger/HighlightedContentLink.stories.svelte
@@ -8,15 +8,15 @@
     title: 'Innhold/Fremhevet innhold',
     args: {
       text: `<p>Haster det? Er det fare for at dyr dør eller blir påført store belastninger. Ring mattilsynet på tlf. <a class='mt-link' href='tel:22400000'>22 40 00 00</a> eller politiet <a class='mt-link' href='tel:02800'>02800</a></p>`,
-      title: `<h3 class='mt-h3 heading'>Tilsyn med småfe</h3>`,
-      shortTitle: `<h3 class='heading'>Tilsyn med småfe</h3>`,
+      title: 'Dette er tittelen',
+      shortTitle: 'Dette er kort tittel',
       disableJs: false,
       disableCss: false
     },
     argTypes: {
-      text: { control: 'string' },
-      title: { control: 'string' },
-      shortTitle: { control: 'string' },
+      text: { control: 'text' },
+      title: { control: 'text' },
+      shortTitle: { control: 'text' },
       disableJs: { control: 'boolean' },
       disableCss: { control: 'boolean' }
     }
@@ -24,7 +24,7 @@
 </script>
 
 <Story name="Normal">
-  {#snippet children({ text, disableCss, title, shortTitle, date })}
+  {#snippet children({ text, disableCss, title, shortTitle })}
     <main use:wrapInShadowDom={disableCss} class="mt-main container layout-flex-col">
       <h1 class="mt-h1">Fremhevet innhold med bilde</h1>
       <section class="layout-flex-col">
@@ -33,8 +33,8 @@
           href="https://www.mattilsynet.no"
           image={{ src: imageFile, alt: 'Her er alt tekst til bildet' }}
           headingClass="mt-h3"
-          title="Dette er tittelen"
-          shortTitle="Dette er kort tittel">
+          {title}
+          {shortTitle}>
           {@html text}
         </HighlightedContentLink>
       </section>
@@ -45,8 +45,8 @@
           displayType="normal"
           href="https://www.mattilsynet.no"
           headingClass="mt-h3"
-          title="Dette er tittelen"
-          shortTitle="Dette er kort tittel">
+          {title}
+          {shortTitle}>
           {@html text}
         </HighlightedContentLink>
       </section>
@@ -55,7 +55,7 @@
 </Story>
 
 <Story name="CTA type">
-  {#snippet children({ text, disableCss, title, shortTitle, date })}
+  {#snippet children({ text, disableCss, title, shortTitle })}
     <main use:wrapInShadowDom={disableCss} class="mt-main container layout-flex-col">
       <h1 class="mt-h1">Fremhevet innhold CTA type</h1>
       <section class="layout-flex-col">
@@ -64,8 +64,8 @@
           href="https://www.mattilsynet.no"
           image={{ src: imageFile, alt: 'Her er alt tekst til bildet' }}
           headingClass="mt-h3"
-          title="Dette er tittelen"
-          shortTitle="Dette er kort tittel">
+          {title}
+          {shortTitle}>
           {@html text}
         </HighlightedContentLink>
       </section>
@@ -76,8 +76,8 @@
           href="https://www.nav.no"
           image={{ src: imageFile, alt: 'Her er alt tekst til bildet' }}
           headingClass="mt-h3"
-          title="Dette er tittelen"
-          shortTitle="Dette er kort tittel">
+          {title}
+          {shortTitle}>
           {@html text}
         </HighlightedContentLink>
       </section>
@@ -85,7 +85,7 @@
   {/snippet}
 </Story>
 <Story name="Campaign">
-  {#snippet children({ text, disableCss, title, shortTitle, date })}
+  {#snippet children({ text, disableCss, title, shortTitle })}
     <h2 class="mt-h2">Fremhevet innhold - promotion</h2>
     <main
       use:wrapInShadowDom={disableCss}
@@ -97,8 +97,8 @@
           class="col-1-span-12 layout-grid layout-grid--column-12"
           image={{ src: imageFile, alt: 'Her er alt tekst til bildet' }}
           headingClass="mt-h3"
-          title="Dette er tittelen"
-          shortTitle="Dette er kort tittel">
+          {title}
+          {shortTitle}>
           {@html text}
         </HighlightedContentLink>
       </section>
