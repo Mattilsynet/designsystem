b6c7e5f5334c60543521c346a94ae9d64e9c886e
Amund Lindstøl <alindstol@gmail.com>
2024-12-13 08:59:59 +0100

    MTP-2803: migrated Select

--- mt-kit/core/svelte/src/lib/svelte/components/form/Select.svelte
+++ mt-kit/core/svelte/src/lib/svelte/components/form/Select.svelte
@@ -1,33 +1,50 @@
-<!-- @migration-task Error while migrating Svelte code: Can't migrate code with beforeUpdate. Please migrate by hand. -->
-<script context="module" lang="ts">
+<script module lang="ts">
   let instanceCounter = 0
 </script>
 
 <script lang="ts">
   import InputError from './InputErrorMessage.svelte'
-  import type { ErrorDetail } from '../../../ts/types'
-  import { createInputAriaDescribedby } from '../../../ts/utils'
+  import type { ErrorDetail } from '$lib/ts'
+  import { createInputAriaDescribedby } from '$lib/ts'
   import Label from './Label.svelte'
-  import { beforeUpdate } from 'svelte'
-  export let name: string
-  export let label: string
-  export let value: string | undefined
-  export let error: ErrorDetail | undefined
-  export let helpText: string | undefined
-  export let isRequired: boolean | undefined = undefined
-  export let options: Array<{ value: string; text: string }> = []
-  export let textOptional: string | undefined
-  export let showOptionalText: boolean = true
-  export let hiddenErrorText: string | undefined
+  import { tick } from 'svelte'
+
+  interface Props {
+    name: string
+    label: string
+    value: string | undefined
+    error: ErrorDetail | undefined
+    helpText: string | undefined
+    isRequired?: boolean | undefined
+    options?: Array<{ value: string; text: string }>
+    textOptional: string | undefined
+    showOptionalText?: boolean
+    hiddenErrorText: string | undefined
+  }
+
+  let {
+    name,
+    label,
+    value = $bindable(),
+    error,
+    helpText,
+    isRequired = undefined,
+    options = [],
+    textOptional,
+    showOptionalText = true,
+    hiddenErrorText
+  }: Props = $props()
 
   const selectId = `ui-select-${instanceCounter++}`
   let isInitialized = false
 
-  beforeUpdate(() => {
-    if (value === undefined && !isInitialized && document) {
-      value = document?.querySelector(`input[name="${name}"]:checked`)?.value
-      isInitialized = true
-    }
+  $effect.pre(() => {
+    tick().then(() => {
+      if (value === undefined && !isInitialized && document) {
+        value = document?.querySelector<HTMLInputElement>(`input[name="${name}"]:checked`)?.value
+        isInitialized = true
+      }
+    })
   })
 </script>
 
@@ -49,8 +66,7 @@
   bind:value
   class="form-field"
   aria-required={isRequired}
-  aria-describedby={createInputAriaDescribedby(helpText ? name : undefined, error)}
->
+  aria-describedby={createInputAriaDescribedby(helpText ? name : undefined, error)}>
   {#each options as option (option.value)}
     <option value={option.value}>
       {option.text}
