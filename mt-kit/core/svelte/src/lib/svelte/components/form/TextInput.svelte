4643e0fcfb100ff9d363e215d19e3b97f4f69ea2
Lisa <halvorsen.lisa@gmail.com>
2025-02-18 13:04:43 +0100

    Update input styling

--- mt-kit/core/svelte/src/lib/svelte/components/form/TextInput.svelte
+++ mt-kit/core/svelte/src/lib/svelte/components/form/TextInput.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-  import InputError from './InputErrorMessage.svelte'
-  import type { AutocompleteType, CountCharsParams, ErrorDetail, InputModeType } from '$lib/ts'
-  import { countCharacters, createInputAriaDescribedby, errorOnTooManyCharacters } from '$lib/ts'
+  import type { AutocompleteType, ErrorDetail, InputModeType } from '$lib/ts'
   import Label from './Label.svelte'
   import { tick } from 'svelte'
+  import { styles } from '@mattilsynet/design'
 
   interface Props {
     value?: string
@@ -56,36 +55,39 @@
   })
 </script>
 
-<!-- TODO check if we can remove wrapping after svelte5 upgrade. See MTP-2784 -->
-<div class="mt-form">
-  <Label for={name} {isRequired} {textOptional} {showOptionalText} class={labelClass}
-    >{label}</Label>
+<div
+  class="{styles.field} field-wrapper"
+  data-size="md"
+  data-required="hidden"
+  style={`--mtds-text-count-under: '%d ${countCharactersLeftLabel}'; --mtds-text-count-over: '%d ${countCharactersTooManyLabel}';`}>
+  <Label for={name} {isRequired} {textOptional} {showOptionalText} class={labelClass}>
+    {label}
+  </Label>
 
   {#if helpText}
-    <div id={`${name}-hint`} class="hint">
+    <p>
       {@html helpText}
-    </div>
+    </p>
   {/if}
 
   {#if error}
-    <InputError {...error} {hiddenErrorText} />
+    <p class={styles.validation}>
+      {error.message}
+    </p>
   {/if}
 
   <input
     id={name}
     {name}
-    use:countCharacters={countCharsParams}
-    oninput={e => {
-      error =
-        errorOnTooManyCharacters(e, countCharsParams, name, tooManyCharactersErrorText) || error
-    }}
-    class="mt-input form-field {inputClass}"
+    class="{styles.input} {inputClass}"
     bind:value
     class:error
     aria-required={isRequired || undefined}
-    aria-describedby={createInputAriaDescribedby(helpText ? name : undefined, error, maxlength)}
-    aria-invalid={!!error}
     {inputmode}
     {placeholder}
     {autocomplete} />
+
+  {#if maxlength}
+    <p data-count={maxlength}></p>
+  {/if}
 </div>
