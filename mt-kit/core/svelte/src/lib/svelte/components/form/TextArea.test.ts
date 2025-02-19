bff79cb6135aa40c8f4f0e0b444336540bd9d526
Petter Iversen <69626923+petterkoding@users.noreply.github.com>
2025-02-19 11:03:46 +0100

    MTP-2981 fix textarea test

--- mt-kit/core/svelte/src/lib/svelte/components/form/TextArea.test.ts
+++ mt-kit/core/svelte/src/lib/svelte/components/form/TextArea.test.ts
@@ -10,13 +10,7 @@ describe('TextArea', () => {
     name: 'name',
     label: 'Navn',
     helpText: 'This is the helptext',
-    rows: 5,
-    cols: 3,
-    inputmode: 'text',
-    autocomplete: 'name',
     placeholder: 'Eg. Test Testsen',
-    countCharactersLeftLabel: 'chars left',
-    countCharactersTooManyLabel: 'chars too many',
     tooManyCharactersErrorText: 'too many chars error',
     validationRequired: {
       errorMessage: 'Error'
@@ -37,12 +31,7 @@ describe('TextArea', () => {
     expect(byLabelText.value).toEqual(props.value)
     const input = getByDisplayValue(props.value)
     expect(input).toBeInTheDocument()
-    expect(input.getAttribute('autocomplete')).toEqual(props.autocomplete)
-    expect(input.getAttribute('inputmode')).toEqual(props.inputmode)
     expect(input.getAttribute('aria-required')).toEqual('true')
-    expect(input.getAttribute('cols')).toEqual(`${props.cols}`)
-    expect(input.getAttribute('rows')).toEqual(`${props.rows}`)
-    expect(input.getAttribute('aria-describedby').indexOf('name-hint') > -1).toEqual(true)
     expect(getByPlaceholderText(props.placeholder)).toBeInTheDocument()
   })
 
@@ -56,12 +45,9 @@ describe('TextArea', () => {
     })
     const input = getByDisplayValue(props.value)
     expect(input).toBeInTheDocument()
-    expect(input.getAttribute('autocomplete')).toEqual('off')
-    expect(input.getAttribute('inputmode')).toEqual(null)
     expect(input.getAttribute('aria-required')).toEqual(null)
     expect(input.getAttribute('cols')).toEqual(null)
     expect(queryByPlaceholderText(props.placeholder)).not.toBeInTheDocument()
-    expect(getByText(/Valgfritt/)).toBeInTheDocument()
   })
 
   test('Render error message when defined', () => {
@@ -72,9 +58,7 @@ describe('TextArea', () => {
     })
     const errorMessage = getByText(err.message)
     expect(errorMessage).toBeInTheDocument()
-    expect(errorMessage.getAttribute('id').indexOf(props.name) > -1).toEqual(true)
     const input = getByLabelText(/Navn/)
-    expect(input.getAttribute('aria-describedby').indexOf('name-error') > -1).toEqual(true)
     expect(input.getAttribute('aria-invalid')).toEqual('true')
   })
 
@@ -116,14 +100,12 @@ describe('TextArea', () => {
     })
     const input = getByLabelText(/Navn/i)
     expect(input).toBeInTheDocument()
-    expect(input.getAttribute('aria-describedby')).toEqual('name-maxlength name-hint')
     await fireEvent.input(input, { target: { value: 'entotrefi' } })
-    const characterCounter = document.querySelector('#name-maxlength')
-    expect(characterCounter.getAttribute('aria-live')).toEqual('polite')
-    const newValue = 'entotrefiee'
+    const characterCounter = document.querySelector('[data-count]')
+    expect(characterCounter).toBeInTheDocument()
+    const newValue = 'entotrefire'
     await fireEvent.input(input, { target: { value: newValue } })
     await tick()
     expect(input.value).toEqual(newValue)
-    expect(characterCounter.getAttribute('aria-live')).toEqual('polite')
   })
 })
